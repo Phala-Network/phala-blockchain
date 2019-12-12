b6faea199ed44c20bb4417a2644cfcdf0ff42622
h4x3rotab <h4x3rotab@gmail.com>
2019-12-13 00:38:09 +0800

    Working phost bridge

--- phost/src/main.rs
+++ phost/src/main.rs
@@ -1,3 +1,16 @@
+// use tokio::io;
+// use tokio::prelude::*;
+use futures_03::compat::Future01CompatExt;
+use tokio::time::delay_for;
+use std::time::Duration;
+
+use serde::{Serialize, Deserialize};
+
+extern crate hyper;
+use hyper::Client as HttpClient;
+use hyper::{Body, Method, Request};
+use bytes::buf::BufExt as _;
+
 use pnode_runtime;
 use sp_rpc::number::NumberOrHex;
 use codec::{Encode, Decode};
@@ -6,94 +19,217 @@ use sp_runtime::{
     OpaqueExtrinsic
 };
 
-use sc_finality_grandpa::GrandpaJustification;
+// use sc_finality_grandpa::GrandpaJustification;
+
+mod error;
+use crate::error::Error;
 
 type Runtime = pnode_runtime::Runtime;
 type Header = <Runtime as subxt::system::System>::Header;
 type OpaqueBlock = sp_runtime::generic::Block<Header, OpaqueExtrinsic>;
 type OpaqueSignedBlock = SignedBlock<OpaqueBlock>;
 
-fn create_client() -> (tokio::runtime::Runtime, subxt::Client<Runtime>) {
-    let mut rt = tokio::runtime::Runtime::new().unwrap();
-    let client_future = subxt::ClientBuilder::<Runtime>::new().build();
-    let client = rt.block_on(client_future).expect("Connect to substrate failed");
-    (rt, client)
-}
-
 fn deopaque_signedblock(opaque_block: OpaqueSignedBlock) -> pnode_runtime::SignedBlock {
     let raw_block = Encode::encode(&opaque_block);
     pnode_runtime::SignedBlock::decode(&mut raw_block.as_slice()).expect("Block decode failed")
 }
 
- fn print_jutification(justification: &Vec<u8>) {
-    let grandpa_j = match GrandpaJustification::<pnode_runtime::Block>::decode(&mut justification.as_slice()) {
-        Ok(j) => j,
-        Err(err) => {
-            println!("Err: {:?}", err);
-            return;
-        }
-    };
-    println!("GrandpaJustification:: <private>");
-    // println!("Justification: {{ round: {}, commit: {:?}, votes_ancestries: {:?} }}",
-    //          grandpa_j.round,
-    //          grandpa_j.commit,
-    //          grandpa_j.votes_ancestries);
- }
+// fn print_jutification(justification: &Vec<u8>) {
+//     let grandpa_j = match GrandpaJustification::<pnode_runtime::Block>::decode(&mut justification.as_slice()) {
+//         Ok(j) => j,
+//         Err(err) => {
+//             println!("Err: {:?}", err);
+//             return;
+//         }
+//     };
+//     println!("GrandpaJustification:: <private>");
+//     // println!("Justification: {{ round: {}, commit: {:?}, votes_ancestries: {:?} }}",
+//     //          grandpa_j.round,
+//     //          grandpa_j.commit,
+//     //          grandpa_j.votes_ancestries);
+// }
 
-fn get_block_at(rt: &mut tokio::runtime::Runtime, client: &subxt::Client<Runtime>, h: Option<u32>)
-        -> Option<pnode_runtime::SignedBlock> {
+async fn get_block_at(client: &subxt::Client<Runtime>, h: Option<u32>)
+        -> Result<pnode_runtime::SignedBlock, Error> {
     let pos = match h {
         Some(h) => Some(NumberOrHex::Number(h)),
         None => None
     };
-    let hash = match rt.block_on(client.block_hash(pos)).unwrap() {
+    let hash = match client.block_hash(pos).compat().await? {
         Some(hash) => hash,
-        None => { eprintln!("Block hash not found!"); return None }
+        None => { eprintln!("Block hash not found!"); return Err(Error::BlockHashNotFound()) }
     };
-    println!("hash: {:?}", hash);
+    println!("get_block_at: Got block {:?} hash {:?}", h, hash);
 
-    let opaque_block = match rt.block_on(client.block(Some(hash))).unwrap() {
+    let opaque_block = match client.block(Some(hash)).compat().await? {
         Some(block) => block,
-        None => { eprintln!("Block not found"); return None },
+        None => { eprintln!("Block not found"); return Err(Error::BlockNotFound()) },
     };
 
-    let raw_block = Encode::encode(&opaque_block);
-    println!("raw block: {}", hex::encode(&raw_block));
+    // let raw_block = Encode::encode(&opaque_block);
+    // println!("raw block: {}", hex::encode(&raw_block));
 
     let block = deopaque_signedblock(opaque_block);
-    println!("block: {:?}", block);
+    // println!("block: {:?}", block);
 
-    Some(block)
+    Ok(block)
 }
 
-fn print_metadata(_rt: &mut tokio::runtime::Runtime, client: &subxt::Client<Runtime>) {
-    let metadata = client.metadata();
-    println!("Metadata: {:?}", metadata);
+#[derive(Serialize, Deserialize, Debug)]
+struct SignedResp {
+    payload: String,
+    status: String,
+    signature: String
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Nonce {
+    value: u32,
+}
+
+impl Nonce {
+    fn new() -> Nonce {
+        Nonce { value: rand::random::<u32>() }
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Null {}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct GetInfo {
+    input: Null,
+    nonce: Nonce
+}
+
+impl GetInfo {
+    fn new() -> GetInfo {
+        GetInfo {
+            input: Null {},
+            nonce: Nonce::new()
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct GetInfoResp {
+    blocknum: pnode_runtime::BlockNumber,
+    initialized: bool,
+    public_key: String
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct SyncBlockInput {
+    // base64 encoded raw SignedBlock
+    data: String
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct SyncBlock {
+    input: SyncBlockInput,
+    nonce: Nonce
+}
+
+impl SyncBlock {
+    fn new(raw_block: String) -> SyncBlock {
+        SyncBlock {
+            input: SyncBlockInput { data: raw_block },
+            nonce: Nonce::new()
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct SyncBlockResp {
+    synced_to: pnode_runtime::BlockNumber
+}
+
+const PRUNTIME_RPC_BASE: &'static str = "http://172.20.20.211:8000";
+
+async fn req<T>(command: &str, param: &T) -> Result<SignedResp, Error>  where T: Serialize {
+    let client = HttpClient::new();
+    let endpoint = format!("{}/{}", PRUNTIME_RPC_BASE, command);
+
+    let body_json = serde_json::to_string(param)?;
+
+    let req = Request::builder()
+        .method(Method::POST)
+        .uri(endpoint)
+        .header("content-type", "application/json")
+        .body(Body::from(body_json))?;
+    
+    let res = client.request(req).await?;
+
+    println!("Response: {}", res.status());
+
+    let body = hyper::body::aggregate(res.into_body()).await?;
+    let signed_resp: SignedResp = serde_json::from_reader(body.reader())?;
+
+    // TODO: validate the response from pRuntime
+
+    Ok(signed_resp)
+}
+
+async fn req_get_info() -> Result<GetInfoResp, Error> {
+    let resp = req("get_info", &GetInfo::new()).await?;
+    let result: GetInfoResp = serde_json::from_str(&*(resp.payload)).unwrap();
+    Ok(result)
+}
+
+async fn req_sync_block(block: &pnode_runtime::SignedBlock) -> Result<SyncBlockResp, Error> {
+    let raw_block = Encode::encode(block);
+    let resp = req("sync_block", &SyncBlock::new(base64::encode(&raw_block))).await?;
+    println!("req_sync_block: {:?}", resp);
+    let result: SyncBlockResp = serde_json::from_str(&*(resp.payload)).unwrap();
+    Ok(result)
+}
+
+async fn bridge() -> Result<(), Error> {
+    // Connect to substrate
+    let client = subxt::ClientBuilder::<Runtime>::new().build().compat().await?;
+
+    let mut info = req_get_info().await?;
+    if !info.initialized {
+        // TODO: request RA and register to chain
+        println!("pRuntime not initialized. TODO: init it!");
+    }
+
+    loop {
+        println!("pRuntime get_info response: {:?}", info);
+        let block_tip = get_block_at(&client, None).await?;
+        // info.blocknum is the next needed block
+        println!("try to upload block from {} to {}", info.blocknum, block_tip.block.header.number);
+
+        // check if pRuntime has already reached the chain tip.
+        if info.blocknum > block_tip.block.header.number {
+            // yes, so wait for new blocks...
+            println!("waiting for new blocks");
+            delay_for(Duration::from_millis(5000)).await;
+            continue;
+        }
+
+        // no, then catch up to the chain tip
+        for h in info.blocknum ..= block_tip.block.header.number {
+            let block = get_block_at(&client, Some(h)).await?;
+            let r = req_sync_block(&block).await?;
+            println!("feeded block {} into pRuntime: {:?}", block.block.header.number, r);
+        }
+
+        // update the latest pRuntime state
+        info = req_get_info().await?;
+    }
+}
+
+async fn async_main() {
+    // start the bridge
+    let r = bridge().await;
+    println!("bridge() exited with result: {:?}", r);
+    // TODO: when got any error, we should wait and retry until it works just like a daemon.
 }
 
 fn main() {
-    let (mut rt, client) = create_client();
-
-    print_metadata(&mut rt, &client);
-
-    /*
-    loop {
-        let runtime_tip = await get_pruntime_height();
-        loop /== every dt ==/ {
-            let chain_tip = await get_chain_height();
-            for h in (runtime_tip + 1)..(chain_tip) {
-                block = get_block_at(&mut rt, &client, Some(h));
-                feed_pruntime(block);
-            }
-        }
-    }
-    */
-
-    let signed_tip = get_block_at(&mut rt, &client, None).expect("Can't get block tip");
-
-    for i in 1..3 {
-        println!("--");
-        let h = signed_tip.block.header.number - i;
-        get_block_at(&mut rt, &client, Some(h));
-    }
+    // tokio 0.1 compatible construction
+    use tokio_compat::runtime;
+    let mut rt = runtime::Runtime::new().unwrap();
+    rt.block_on_std(async_main());
 }
