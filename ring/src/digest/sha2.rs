9123ad34ef30fdeb4b321d3ebf6d7d832abd35c7
jasl <jasl9187@hotmail.com>
2019-12-24 23:15:22 +0800

    follow ring upstream

--- ring/src/digest/sha2.rs
+++ ring/src/digest/sha2.rs
@@ -159,10 +159,8 @@ pub(super) trait Word:
 
     type InputBytes: Copy;
 
-    //#[inline(always)]
     fn from_be_bytes(input: Self::InputBytes) -> Self;
 
-    //#[inline]
     fn rotr(self, count: u32) -> Self;
 }
 
@@ -279,7 +277,7 @@ impl Word for Wrapping<u64> {
         Wrapping(u64::from_be_bytes(input))
     }
 
-    #[inline]
+    #[inline(always)]
     fn rotr(self, count: u32) -> Self {
         Wrapping(self.0.rotate_right(count))
     }
