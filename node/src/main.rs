82bc31a3aa27db94b4763a15a4105a0f4bcc6df7
jasl <jasl9187@hotmail.com>
2020-04-17 03:21:55 +0800

    pass node compiling

--- node/src/main.rs
+++ node/src/main.rs
@@ -8,16 +8,5 @@ mod cli;
 mod command;
 
 fn main() -> sc_cli::Result<()> {
-	let version = sc_cli::VersionInfo {
-		name: "Phala Blockchain Node",
-		commit: env!("VERGEN_SHA_SHORT"),
-		version: env!("CARGO_PKG_VERSION"),
-		executable_name: "phala-blockchain",
-		author: "Phala",
-		description: "Phala network",
-		support_url: "support.anonymous.an",
-		copyright_start_year: 2017,
-	};
-
-	command::run(version)
+	command::run()
 }
