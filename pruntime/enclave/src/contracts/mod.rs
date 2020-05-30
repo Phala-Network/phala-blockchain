1fcfa4bb501f2bd79d7deaa7b879a8fd438cdf6e
goldenfiredo <goldenfiredo@gmail.com>
2020-05-30 20:58:54 +0800

    remove space

--- pruntime/enclave/src/contracts/mod.rs
+++ pruntime/enclave/src/contracts/mod.rs
@@ -22,70 +22,70 @@ pub const BALANCE: ContractId = 2;
 pub const ASSETS: ContractId = 3;
 
 pub trait Contract<Cmd, QReq, QResp>: Serialize + DeserializeOwned + Debug
-    where
-        Cmd: Serialize + DeserializeOwned + Debug,
-        QReq: Serialize + DeserializeOwned + Debug,
-        QResp: Serialize + DeserializeOwned + Debug
+where
+  Cmd: Serialize + DeserializeOwned + Debug,
+  QReq: Serialize + DeserializeOwned + Debug,
+  QResp: Serialize + DeserializeOwned + Debug
 {
-    fn id(&self) -> ContractId;
-    fn handle_command(&mut self, origin: &chain::AccountId, txref: &TxRef, cmd: Cmd) -> TransactionStatus;
-    fn handle_query(&mut self, origin: Option<&chain::AccountId>, req: QReq) -> QResp;
+  fn id(&self) -> ContractId;
+  fn handle_command(&mut self, origin: &chain::AccountId, txref: &TxRef, cmd: Cmd) -> TransactionStatus;
+  fn handle_query(&mut self, origin: Option<&chain::AccountId>, req: QReq) -> QResp;
 }
 
 pub fn account_id_from_hex(accid_hex: &String) -> Result<chain::AccountId, ()> {
-    use core::convert::TryFrom;
-    let bytes = crate::hex::decode_hex(accid_hex);
-    chain::AccountId::try_from(bytes.as_slice())
+  use core::convert::TryFrom;
+  let bytes = crate::hex::decode_hex(accid_hex);
+  chain::AccountId::try_from(bytes.as_slice())
 }
 
 pub mod serde_balance {
-    use crate::std::string::{String, ToString};
-    use crate::std::str::FromStr;
-    use serde::{de, Serialize, Deserialize, Serializer, Deserializer};
+  use crate::std::string::{String, ToString};
+  use crate::std::str::FromStr;
+  use serde::{de, Serialize, Deserialize, Serializer, Deserializer};
 
-    pub fn serialize<S>(value: &chain::Balance, serializer: S) -> Result<S::Ok, S::Error>
-        where S: Serializer {
-        let s = value.to_string();
-        String::serialize(&s, serializer)
-    }
-    pub fn deserialize<'de, D>(deserializer: D) -> Result<chain::Balance, D::Error>
-        where D: Deserializer<'de> {
-        let s = String::deserialize(deserializer)?;
-        chain::Balance::from_str(&s).map_err(de::Error::custom)
-    }
+  pub fn serialize<S>(value: &chain::Balance, serializer: S) -> Result<S::Ok, S::Error>
+    where S: Serializer {
+    let s = value.to_string();
+    String::serialize(&s, serializer)
+  }
+  pub fn deserialize<'de, D>(deserializer: D) -> Result<chain::Balance, D::Error>
+    where D: Deserializer<'de> {
+    let s = String::deserialize(deserializer)?;
+    chain::Balance::from_str(&s).map_err(de::Error::custom)
+  }
 }
 
 #[derive(Default, Debug, Ord, PartialOrd, Eq, PartialEq, Clone)]
 pub struct AccountIdWrapper( pub chain::AccountId );
 
 impl<'a> AccountIdWrapper {
-    fn from(b: &'a [u8]) -> Self {
-        let mut a = AccountIdWrapper::default();
-        use core::convert::TryFrom;
-        a.0 = sp_core::crypto::AccountId32::try_from(b).unwrap();
-        a
-    }
-    fn from_hex(s: &str) -> Self {
-        let bytes = crate::hex::decode_hex(s);  // TODO: error handling
-        AccountIdWrapper::from(&bytes)
-    }
-    fn to_string(&self) -> String {
+  fn from(b: &'a [u8]) -> Self {
+    let mut a = AccountIdWrapper::default();
+    use core::convert::TryFrom;
+    a.0 = sp_core::crypto::AccountId32::try_from(b).unwrap();
+    a
+  }
+  fn from_hex(s: &str) -> Self {
+    let bytes = crate::hex::decode_hex(s);  // TODO: error handling
+    AccountIdWrapper::from(&bytes)
+  }
+  fn to_string(&self) -> String {
         crate::hex::encode_hex_compact(self.0.as_ref())
     }
 }
 
 impl Serialize for AccountIdWrapper {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-        where S: Serializer,
-    {
-        let data_hex = self.to_string();
-        serializer.serialize_str(&data_hex)
-    }
+  fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where S: Serializer,
+  {
+    let data_hex = self.to_string();
+    serializer.serialize_str(&data_hex)
+  }
 }
 
 impl<'de> Deserialize<'de> for AccountIdWrapper{
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-        where D: Deserializer<'de>
+  fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where D: Deserializer<'de>
     {
         deserializer.deserialize_str(AcidVisitor)
     }
@@ -94,20 +94,20 @@ impl<'de> Deserialize<'de> for AccountIdWrapper{
 struct AcidVisitor;
 
 impl<'de> Visitor<'de> for AcidVisitor {
-    type Value = AccountIdWrapper;
+  type Value = AccountIdWrapper;
 
-    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
-        formatter.write_str("AccountID is the hex of [u8;32]")
-    }
+  fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+    formatter.write_str("AccountID is the hex of [u8;32]")
+  }
 
-    fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
-        where E: de::Error,
-    {
-        if v.len() == 64 {
-            let bytes = crate::hex::decode_hex(v);  // TODO: error handling
-            Ok(AccountIdWrapper::from(&bytes))
-        } else {
-            Err(E::custom(format!("AccountId hex length wrong: {}", v)))
-        }
+  fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
+    where E: de::Error,
+  {
+    if v.len() == 64 {
+      let bytes = crate::hex::decode_hex(v);  // TODO: error handling
+      Ok(AccountIdWrapper::from(&bytes))
+    } else {
+      Err(E::custom(format!("AccountId hex length wrong: {}", v)))
     }
+  }
 }
--- pruntime/enclave/src/types.rs
+++ pruntime/enclave/src/types.rs
@@ -10,41 +10,41 @@ extern crate runtime as chain;
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct TxRef {
-    pub blocknum: chain::BlockNumber,
-    pub index: u32,
-    pub tx_hash: String,
+  pub blocknum: chain::BlockNumber,
+  pub index: u32,
+  pub tx_hash: String,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub enum Payload {
-    Plain(String),
-    Cipher(AeadCipher),
+  Plain(String),
+  Cipher(AeadCipher),
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct SignedQuery {
-    pub query_payload: String,
-    pub origin: Option<Origin>
+  pub query_payload: String,
+  pub origin: Option<Origin>
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Query<T> {
-    pub contract_id: u32,
-    pub nonce: u32,
-    pub request: T
+  pub contract_id: u32,
+  pub nonce: u32,
+  pub request: T
 }
 impl<T> Query<T> where T : Serialize + DeserializeOwned + Debug + Clone {}
 
 pub type OpaqueQuery = Query<serde_json::Value>;
 pub fn deopaque_query<T>(q: OpaqueQuery) -> Result<Query<T>, Error>
-    where T: Serialize + DeserializeOwned + Debug + Clone {
-    Ok(Query {
-        contract_id: q.contract_id,
-        nonce: q.nonce,
-        request: serde_json::from_value(q.request).map_err(|_| Error::DecodeError)?
-    })
+  where T: Serialize + DeserializeOwned + Debug + Clone {
+  Ok(Query {
+    contract_id: q.contract_id,
+    nonce: q.nonce,
+    request: serde_json::from_value(q.request).map_err(|_| Error::DecodeError)?
+  })
 }
 
 pub enum Error {
-    DecodeError,
+  DecodeError,
 }
