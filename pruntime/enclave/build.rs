75aa9a8a223c8a3c4c172631cb375f88af7ba1cc
h4x3rotab <h4x3rotab@gmail.com>
2020-10-04 23:51:41 +0800

    pRuntime: add missing slash in sigrl url

--- pruntime/enclave/build.rs
+++ pruntime/enclave/build.rs
@@ -10,13 +10,13 @@ fn main () {
     match ias_env.as_ref() {
         "PROD" => {
             println!("cargo:rustc-env=IAS_HOST=api.trustedservices.intel.com");
-            println!("cargo:rustc-env=IAS_SIGRL_ENDPOINT=/sgx/attestation/v4/sigrl");
+            println!("cargo:rustc-env=IAS_SIGRL_ENDPOINT=/sgx/attestation/v4/sigrl/");
             println!("cargo:rustc-env=IAS_REPORT_ENDPOINT=/sgx/attestation/v4/report");
         }
         _ => {
             // DEV by default
             println!("cargo:rustc-env=IAS_HOST=api.trustedservices.intel.com");
-            println!("cargo:rustc-env=IAS_SIGRL_ENDPOINT=/sgx/dev/attestation/v4/sigrl");
+            println!("cargo:rustc-env=IAS_SIGRL_ENDPOINT=/sgx/dev/attestation/v4/sigrl/");
             println!("cargo:rustc-env=IAS_REPORT_ENDPOINT=/sgx/dev/attestation/v4/report");
         }
     }
