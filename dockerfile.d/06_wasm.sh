9a29ff4e109d0dd49c0c3c28357d79f35d815a46
Jiacheng Zhang <gh@kt.je>
2020-10-08 23:30:27 +0800

    docker: stick to a working nightly

--- dockerfile.d/06_wasm.sh
+++ dockerfile.d/06_wasm.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
 echo "*** Initializing WASM build environment"
-/root/.cargo/bin/rustup target add wasm32-unknown-unknown --toolchain "${RUST_TOOLCHAIN}" && \
-/root/.cargo/bin/rustup target add wasm32-unknown-unknown --toolchain nightly
+if [ -z ${RUST_TOOLCHAIN+x} ]
+then
+  /root/.cargo/bin/rustup target add wasm32-unknown-unknown --toolchain "${RUST_TOOLCHAIN}"
+else
+  /root/.cargo/bin/rustup target add wasm32-unknown-unknown --toolchain nightly-2020-09-27
+fi
