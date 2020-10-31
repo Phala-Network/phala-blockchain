88ef5e494e488007508738e4f26c10f1a12f8caa
liwei <luswar@163.com>
2020-11-01 01:34:10 +0800

    Change claim pallet name (ClaimMoudle => PhaClaim)

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -924,7 +924,6 @@ impl pallet_phala::Trait for Runtime {
 impl pallet_claim::Trait for Runtime {
 	type Event = Event;
 	type Currency = Balances;
-
 }
 
 construct_runtime!(
@@ -942,7 +941,7 @@ construct_runtime!(
 		Balances: pallet_balances::{Module, Call, Storage, Config<T>, Event<T>},
 		TransactionPayment: pallet_transaction_payment::{Module, Storage},
 		PhalaModule: pallet_phala::{Module, Call, Config<T>, Storage, Event<T>}, // Before Staking to ensure init sequence
-		ClaimModule: pallet_claim::{Module, Call, Storage, Event<T>},
+		PhaClaim: pallet_claim::{Module, Call, Storage, Event<T>},
 		Staking: pallet_staking::{Module, Call, Config<T>, Storage, Event<T>, ValidateUnsigned},
 		Session: pallet_session::{Module, Call, Storage, Event, Config<T>},
 		Democracy: pallet_democracy::{Module, Call, Storage, Config, Event<T>},
