56007a53f875bb41d70940bde30a4d230137c09c
h4x3rotab <h4x3rotab@gmail.com>
2020-12-15 17:22:55 +0800

    Update firedrop script

--- scripts/js/firedropDistribution.js
+++ scripts/js/firedropDistribution.js
@@ -28,6 +28,47 @@ const poc3Prize = [{
         "blockNum": 110277,
         "blockHash": "0x50294f31df05190e696241e786c21476e91772847de485b58644fcfefdea880e"
     }
+}, {
+    // Assume amounts are integer
+    data: [
+        ["42wm1sm39gs3ngTGxapLaMUvFQHtjERo7JD6CkZ2VL6KQrpK",38129],
+        ["42qurrwZ53YaP3vtpSZNCrJADDjaUWBf8MiGcA9jQQuUsmcn",12928],
+        ["44i4Dpm79kLPyErbLhy1bhnudBNmQ2brVug6xkjythBYkv1c",20909],
+        ["43k8j5AX9sKmEL195mrCkqb4oPfbpX5VixF9M4MpbCAkUN97",33634],
+        ["44HhYTW3AJF6ZqNG8zgYsF4z8HTUKdxZQ59VxBb68q23q6kk",1263],
+        ["44TtxmTstgf1ns1hBAUBne6uUDueKFY27RJd44A9Kd8G1UE7",45626],
+        ["45mukZ3RhgwL9ZANmawBu6izEa2nnDfLhSsSjXiRsqcBB3ow",4330],
+        ["44FAD7fevqwf8YUx97fuCKGPbuACZS4xr2icZz1E8CxyBStN",7300],
+        ["441bRVk56V1u5vBG56BkQFx1nwJRCeh3GxoRQoGYuEgNp2wy",25373],
+        ["43jnxA4HZPNeU1yRRBucGyNzJgwCkmTNLSeh1Cep5zAwQjXa",21482],
+        ["41AbqJUke2zgtGE6ftY6NfV6jzEEv9PxVu2G9MS8GpTuxvP4",40671],
+        ["43iF6zUPjHRghpfNXNdq4iwUuBP8NL5VdLHFVdhnBstFEvdB",34519],
+        ["41SxLZymSn5tUaRMPbFDrDuYFads13Y4VkAhXxJyat3PL1Hg",9251],
+        ["45qPYPFLytVanq5ThhSEJTY5tPrA7FqfNdtr3YCV7mXkVtSH",10953],
+        ["459BZcn6oTQJfK4pBPFynmrnJRv2rt4V5XGKemeGbjeG5ppS",13589],
+        ["46KSjF38thvjd1VLt2io3LS4EicReyX52vmLkcgkB9WccNH7",18856],
+        ["459C4KLMzm8ZyCjxRynSgVNEoyR8UyNGg5WKjjyc7gPJxSTs",16109],
+        ["43P1V2MGomDrGbpR7q7gUguqdkT9wxWmKRSM7kK42Gv54Xvh",12932],
+        ["3zsAjyMhtLzsonWNdCBgq7bLtZeinDw8VDQJrJBNqWQhdYAb",16719],
+        ["42FkcGa4aANzBPAG3Hz8NpNGqjVxuDiV72JHpqi5jeYqGKv3",15573],
+        ["45C6u5LDk21EG1fhQ2HxtadmUsRdux7fy9G9VokrqE2CofA2",3366],
+        ["3zdR8kg51TRS9LezkvV1dAUYs6yS9CwGRMvkJ45ZJEaHhMw2",12955],
+        ["45UdKNgExq9FH1Nwq3JZerwfhhM655Yra8Z48taEJnW7cUop",15842],
+        ["44qTDccE9oN1ek6LTr6Td8ZHLtstLaBgGw5g9w99wmPUxpNq",409],
+        ["45xJ52kthVuAFTGrDbAySrm3iagnxZaDAktF8xHPMrqMCNhV",7690],
+        ["46MsBwFUD46wLgP8Si1TjS4kruDoQyFhFfTawQwCwNEbCXYF",20972],
+        ["45uquvkE2ij8ERA83HwNoxqh793mJcvXN2kqbaYHv1UpZAuh",80778],
+        ["44Ew2kE76R2E7B4e5QMGhE4FZ7FVtSkTx222UKXThteqMWpS",41101],
+        ["41ofhHnutERPGfcCKb3KkDLyYZn4ZDJUodU8hDJgnaB1iRR8",56162],
+        ["41oHFx9DQ2nJzEAZtaqGEjffnUktPzgsGsrnsKV6CLu7x1ZD",13220],
+        ["41mJagM7FgBhMk8vrojEbC8hEk9Ngoy1zwANMVG29Xs2rLEG",25713],
+        ["45RgbbUak7NbJwPaZ71xzAXoteBrE32MdwdfaeQP5UNrMa9u",28408],
+        ["4449QTQYK9BzuaDWYFte4LnMTZjtg6H7dcrReteXzhToX2nd",13238],
+    ],
+    context: {
+        "blockNum": 252600,
+        "blockHash": "0x7d98d0a541d522125967ed88828899ec6a10fd936be016ed1292866c6926cabd"
+    }
 }];
 
 async function getPayoutAddress (api, hash, stash) {
@@ -42,7 +83,7 @@ async function main () {
     const keyring = new Keyring({ type: 'sr25519' });
     const root = keyring.addFromUri(process.env.PRIVKEY);
 
-    const {data, context} = poc3Prize[0];
+    const {data, context} = poc3Prize[poc3Prize.length - 1];
 
     const argAddrs = [];
     const argAmounts = [];
@@ -52,8 +93,10 @@ async function main () {
         argAmounts.push(new BN(amount).mul(bn1e12));
     }
 
+    console.log('payouts', argAddrs.map((addr, i) => ({addr: addr.toHuman(), amount: data[i][1]})));
+
     await new Promise(resolve => {
-        const unsub = api.tx.sudo.sudo(api.tx.phalaModule.forceAddFire(argAddrs, argAmounts))
+        const unsub = await api.tx.sudo.sudo(api.tx.phalaModule.forceAddFire(argAddrs, argAmounts))
             .signAndSend(root, (result) => {
                 console.log(`Current status is ${result.status}`);
                 if (result.status.isInBlock) {
