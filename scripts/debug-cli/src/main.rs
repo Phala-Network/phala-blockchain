459ec9f11b216e5bba8bee6dd5f19521d21bc8d4
h4x3rotab <h4x3rotab@gmail.com>
2020-12-26 01:28:31 +0800

    debug-cli: add RA quote decode

--- scripts/debug-cli/src/main.rs
+++ scripts/debug-cli/src/main.rs
@@ -9,6 +9,10 @@ enum Cli {
     DecodeWorkerMessage {
         #[structopt(short)]
         hex_data: String,
+    },
+    DecodeRaQuote {
+        #[structopt(short)]
+        b64_data: String,
     }
 }
 
@@ -21,6 +25,18 @@ fn main() {
              let msg: phala_types::SignedWorkerMessage = Decode::decode(&mut data.as_slice())
                 .expect("Failed to decode message");
             println!("Decoded: {:?}", msg);
+        },
+        Cli::DecodeRaQuote { b64_data } => {
+            let quote_body = base64::decode(&b64_data)
+                .expect("Failed to decode b64_data");
+			let mr_enclave = &quote_body[112..144];
+			let mr_signer = &quote_body[176..208];
+			let isv_prod_id = &quote_body[304..306];
+            let isv_svn = &quote_body[306..308];
+            println!("- mr_enclave: {}", hex::encode(&mr_enclave));
+            println!("- mr_signer: {}", hex::encode(&mr_signer));
+            println!("- isv_prod_id: {}", hex::encode(&isv_prod_id));
+            println!("- isv_svn: {}", hex::encode(&isv_svn));
         }
     }
 }
