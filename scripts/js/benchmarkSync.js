2fdde97061a6794c0151cddabb3b35e85916775c
h4x3rotab <h4x3rotab@gmail.com>
2020-12-26 07:20:00 +0800

    Add readproof in benchmarkSync

--- scripts/js/benchmarkSync.js
+++ scripts/js/benchmarkSync.js
@@ -20,6 +20,8 @@ async function main() {
         const h = await api.rpc.chain.getBlockHash(blocknum);
         const _block = await api.rpc.chain.getBlock(h);
         const _event = await api.query.system.events.at(h);
+        const key = api.query.system.events.key();
+        const _proof = await api.rpc.state.getReadProof([key], h);
         blocknum++;
     }
 }
