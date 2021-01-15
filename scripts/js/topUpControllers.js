4551f6685075ab96e5c5249c5296725de039e0fb
h4x3rotab <h4x3rotab@gmail.com>
2021-01-15 22:29:53 +0800

    Update topUpControllers script

--- scripts/js/topUpControllers.js
+++ scripts/js/topUpControllers.js
@@ -5,12 +5,12 @@
  *  - PRIVKEY: sr25519 privkey sURI
  *  - ENDPOINT: full node WS (WSS) RPC endpoint
  *  - TOPUP_PAYOUT_TARGET: the mining payout target account that owns the controllers to top up
- *  - AMOUNT: the amount to top up, integer only
+ *  - TARGET_AMOUNT: top up to the specified amount. no-op if the account has more token
  *  - AMOUNT_MIN: skip the controller if it has this min amount, float accepted
  *  - DRYRUN: 1 means dry-run mode (optional)
  *
  * Example:
- *  TOPUP_PAYOUT_TARGET=<some-payout-address> AMOUNT=10 MIN_AMOUNT=5 node topUpControllers.js
+ *  TOPUP_PAYOUT_TARGET=<some-payout-address> TARGET_AMOUNT=5 node topUpControllers.js
  */
 
 require('dotenv').config();
@@ -33,6 +33,10 @@ async function getBalances (api, addresses) {
     });
 }
 
+function realToBn(f) {
+    return new BN(f * 1e3 | 0).mul(bn1e9);
+}
+
 async function main () {
     const wsProvider = new WsProvider(process.env.ENDPOINT);
     const api = await ApiPromise.create({ provider: wsProvider, types: typedefs });
@@ -41,8 +45,8 @@ async function main () {
     const sender = keyring.addFromUri(process.env.PRIVKEY);
 
     const payoutTarget = process.env.TOPUP_PAYOUT_TARGET;
-    const amount = new BN(process.env.AMOUNT).mul(bn1e12);
-    const minAmount = parseFloat(process.env.MIN_AMOUNT);
+    // const amount = new BN(process.env.AMOUNT).mul(bn1e12);
+    const targetAmount = parseFloat(process.env.TARGET_AMOUNT);
 
     console.log('Getting controllers');
     const entries = await api.query.phalaModule.stashState.entries();
@@ -64,16 +68,19 @@ async function main () {
     console.log('Getting balance');
     const balances = await getBalances(api, controllers);
     const balancesFloat = balances.map(b => b.div(bn1e9).toNumber() / 1e3);
-    const topUpTargets = controllers.filter(
-        (_, idx) => {
-            const shouldTopUp = balancesFloat[idx] <= minAmount;
-            if (shouldTopUp && kVerbose) {
-                console.log(`${idx}: ${balancesFloat[idx]}`)
-            }
-            return shouldTopUp;
-        });
-    console.log(`Found ${topUpTargets.length}`);
-    console.log({topUpTargets});
+    let topUpPlan = controllers
+        .map((address, idx) => ({
+            address,
+            amount: targetAmount - balancesFloat[idx]
+        }))
+        .filter(({amount}) => amount > 0);
+    console.log({topUpPlan});
+    console.log(`Found ${topUpPlan.length}`);
+
+    topUpPlan = topUpPlan.map(({address, amount}) => ({
+        address,
+        amount: realToBn(amount),
+    }));
 
     if (kDryRun) {
         console.log('Exiting because of dryrun');
@@ -82,8 +89,8 @@ async function main () {
 
     await new Promise(async resolve => {
         const unsub = await api.tx.utility.batch(
-            topUpTargets.map(controller =>
-                api.tx.balances.transfer(controller, amount))
+            topUpPlan.map(({address, amount}) =>
+                api.tx.balances.transfer(address, amount))
         ).signAndSend(sender, (result) => {
             console.log(`Current status is ${result.status}`);
             if (result.status.isInBlock) {
