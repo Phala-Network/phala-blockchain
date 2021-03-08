fca6cb34b52a004a4b8320372024a3e7161bc6e6
jasl <jasl9187@hotmail.com>
2021-03-09 02:16:47 +0800

    follow latest substrate

--- standalone/runtime/src/lib.rs
+++ standalone/runtime/src/lib.rs
@@ -1436,6 +1436,14 @@ impl_runtime_apis! {
 		}
 	}
 
+	#[cfg(feature = "try-runtime")]
+	impl frame_try_runtime::TryRuntime<Block> for Runtime {
+		fn on_runtime_upgrade() -> Result<(Weight, Weight), sp_runtime::RuntimeString> {
+			let weight = Executive::try_runtime_upgrade()?;
+			Ok((weight, RuntimeBlockWeights::get().max_block))
+		}
+	}
+
 	#[cfg(feature = "runtime-benchmarks")]
 	impl frame_benchmarking::Benchmark<Block> for Runtime {
 		fn dispatch_benchmark(
