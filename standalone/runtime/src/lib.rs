5a0a333016f155a298d63e24bbc5e87e09c4d2c8
jasl <jasl9187@hotmail.com>
2021-04-15 05:25:47 +0800

    correct SS58Prefix

--- standalone/runtime/src/lib.rs
+++ standalone/runtime/src/lib.rs
@@ -194,7 +194,7 @@ parameter_types! {
 		})
 		.avg_block_initialization(AVERAGE_ON_INITIALIZE_RATIO)
 		.build_or_panic();
-		pub const SS58Prefix: u8 = 42;
+		pub const SS58Prefix: u8 = 30;
 }
 
 const_assert!(NORMAL_DISPATCH_RATIO.deconstruct() >= AVERAGE_ON_INITIALIZE_RATIO.deconstruct());
