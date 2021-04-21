3fbd63f23088e3d4fe41db01342d1303d36f44a8
h4x3rotab <h4x3rotab@gmail.com>
2021-04-21 09:32:17 +0000

    debug-cli: decode BHWE, WorkerSnapshot

--- scripts/debug-cli/src/main.rs
+++ scripts/debug-cli/src/main.rs
@@ -1,4 +1,4 @@
-use codec::Decode;
+use codec::{Encode, Decode};
 use structopt::StructOpt;
 
 // use phala_types;
@@ -24,6 +24,15 @@ enum Cli {
         #[structopt(short)]
         hex_data: String,
     },
+    DecodeWorkerSnapshot {
+        #[structopt(short)]
+        hex_data: String,
+    },
+    DecodeBhwe {
+        #[structopt(short)]
+        b64_data: String,
+    },
+    Test,
 }
 
 fn main() {
@@ -70,6 +79,26 @@ fn main() {
             let hash = header.hash();
             println!("Decoded: {:?}", header);
             println!("Hash: 0x{}", hex::encode(&hash));
+        },
+        Cli::DecodeWorkerSnapshot { hex_data } => {
+            let data = hex::decode(hex_data)
+                .expect("Failed to parse hex_data");
+            let snapshot = phala_types::pruntime::OnlineWorkerSnapshot::<u32, u128>
+                ::decode(&mut data.as_slice());
+            
+            println!("Decoded: {:?}", snapshot);
         }
+        Cli::DecodeBhwe { b64_data } => {
+            use sp_runtime::{
+                generic::Header,
+                traits::BlakeTwo256,
+            };
+            let data = base64::decode(&b64_data)
+                .expect("Failed to decode b64_data");
+            let snapshot = phala_types::pruntime::BlockHeaderWithEvents::<u32, BlakeTwo256, u128>
+                ::decode(&mut data.as_slice());
+            
+            println!("Decoded: {:?}", snapshot);
+        },
     }
 }
