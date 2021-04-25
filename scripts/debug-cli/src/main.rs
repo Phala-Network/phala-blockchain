e060fd82b4ee19f4ec551ade79f5ae00ab7bdf19
h4x3rotab <h4x3rotab@gmail.com>
2021-04-25 04:46:02 +0000

    debug-cli: decode worker message queue

--- scripts/debug-cli/src/main.rs
+++ scripts/debug-cli/src/main.rs
@@ -1,4 +1,4 @@
-use codec::{Encode, Decode};
+use codec::{Decode, Encode};
 use structopt::StructOpt;
 
 // use phala_types;
@@ -10,11 +10,15 @@ enum Cli {
         #[structopt(short)]
         hex_data: String,
     },
+    DecodeWorkerMessageQueue {
+        #[structopt(short)]
+        b64_data: String,
+    },
     DecodePruntimeInfo {
         #[structopt(short)]
         hex_data: String,
         #[structopt(long)]
-        print_field: Option<String>
+        print_field: Option<String>,
     },
     DecodeRaQuote {
         #[structopt(short)]
@@ -32,73 +36,72 @@ enum Cli {
         #[structopt(short)]
         b64_data: String,
     },
-    Test,
 }
 
 fn main() {
     let cli = Cli::from_args();
     match cli {
         Cli::DecodeWorkerMessage { hex_data } => {
-            let data = hex::decode(hex_data)
-                .expect("Failed to parse hex_data");
-             let msg: phala_types::SignedWorkerMessage = Decode::decode(&mut data.as_slice())
-                .expect("Failed to decode message");
+            let data = hex::decode(hex_data).expect("Failed to parse hex_data");
+            let msg: phala_types::SignedWorkerMessage =
+                Decode::decode(&mut data.as_slice()).expect("Failed to decode message");
             println!("Decoded: {:?}", msg);
-        },
-        Cli::DecodePruntimeInfo { hex_data, print_field } => {
-            let data = hex::decode(hex_data)
-                .expect("Failed to parse hex_data");
-            let msg: phala_types::PRuntimeInfo = Decode::decode(&mut data.as_slice())
-                .expect("Failed to decode message");
+        }
+        Cli::DecodeWorkerMessageQueue { b64_data } => {
+            let data = base64::decode(&b64_data).expect("Failed to decode b64_data");
+            let msg: Vec<phala_types::SignedWorkerMessage> =
+                Decode::decode(&mut data.as_slice()).expect("Failed to decode message");
+            println!("Decoded: {:?}", msg);
+        }
+        Cli::DecodePruntimeInfo {
+            hex_data,
+            print_field,
+        } => {
+            let data = hex::decode(hex_data).expect("Failed to parse hex_data");
+            let msg: phala_types::PRuntimeInfo =
+                Decode::decode(&mut data.as_slice()).expect("Failed to decode message");
             match print_field {
                 Some(f) if f == "machine_id" => println!("{}", hex::encode(&msg.machine_id)),
                 _ => println!("Decoded: {:?}", msg),
             }
-        },
+        }
         Cli::DecodeRaQuote { b64_data } => {
-            let quote_body = base64::decode(&b64_data)
-                .expect("Failed to decode b64_data");
-			let mr_enclave = &quote_body[112..144];
-			let mr_signer = &quote_body[176..208];
-			let isv_prod_id = &quote_body[304..306];
+            let quote_body = base64::decode(&b64_data).expect("Failed to decode b64_data");
+            let mr_enclave = &quote_body[112..144];
+            let mr_signer = &quote_body[176..208];
+            let isv_prod_id = &quote_body[304..306];
             let isv_svn = &quote_body[306..308];
             println!("- mr_enclave: {}", hex::encode(&mr_enclave));
             println!("- mr_signer: {}", hex::encode(&mr_signer));
             println!("- isv_prod_id: {}", hex::encode(&isv_prod_id));
             println!("- isv_svn: {}", hex::encode(&isv_svn));
-        },
+        }
         Cli::DecodeHeader { hex_data } => {
-            use sp_runtime::{
-                generic::Header,
-                traits::BlakeTwo256,
-            };
-            let data = hex::decode(hex_data)
-                .expect("Failed to parse hex_data");
+            use sp_runtime::{generic::Header, traits::BlakeTwo256};
+            let data = hex::decode(hex_data).expect("Failed to parse hex_data");
             let header = Header::<u128, BlakeTwo256>::decode(&mut data.as_slice())
                 .expect("Faield to parse Header");
             let hash = header.hash();
             println!("Decoded: {:?}", header);
             println!("Hash: 0x{}", hex::encode(&hash));
-        },
+        }
         Cli::DecodeWorkerSnapshot { hex_data } => {
-            let data = hex::decode(hex_data)
-                .expect("Failed to parse hex_data");
-            let snapshot = phala_types::pruntime::OnlineWorkerSnapshot::<u32, u128>
-                ::decode(&mut data.as_slice());
-            
+            let data = hex::decode(hex_data).expect("Failed to parse hex_data");
+            let snapshot = phala_types::pruntime::OnlineWorkerSnapshot::<u32, u128>::decode(
+                &mut data.as_slice(),
+            );
+
             println!("Decoded: {:?}", snapshot);
         }
         Cli::DecodeBhwe { b64_data } => {
-            use sp_runtime::{
-                generic::Header,
-                traits::BlakeTwo256,
-            };
-            let data = base64::decode(&b64_data)
-                .expect("Failed to decode b64_data");
-            let snapshot = phala_types::pruntime::BlockHeaderWithEvents::<u32, BlakeTwo256, u128>
-                ::decode(&mut data.as_slice());
-            
+            use sp_runtime::{generic::Header, traits::BlakeTwo256};
+            let data = base64::decode(&b64_data).expect("Failed to decode b64_data");
+            let snapshot =
+                phala_types::pruntime::BlockHeaderWithEvents::<u32, BlakeTwo256, u128>::decode(
+                    &mut data.as_slice(),
+                );
+
             println!("Decoded: {:?}", snapshot);
-        },
+        }
     }
 }
