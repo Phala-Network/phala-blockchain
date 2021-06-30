d688f96b856d3dff546dd548690cf58d828bfc5e
Shelven Zhou <favoring.zhou49@gmail.com>
2021-06-29 22:05:45 -0400

    export error in IdentityKey generation and separate introduce of entropy

--- crates/crypto/src/identity_key.rs
+++ crates/crypto/src/identity_key.rs
@@ -1,25 +1,15 @@
 use alloc::vec::Vec;
-use sp_core::{crypto::Pair, ecdsa};
+use sp_core::crypto::{Pair, SecretStringError};
+use sp_core::ecdsa;
 
 pub struct IdentityKey(ecdsa::Pair);
 
 pub type Seed = [u8; 32];
 
 impl IdentityKey {
-    fn generate() -> (Self, Seed) {
-        use rand::RngCore;
-        // TODO.shelven: remove thread_rng() since it requires std support
-        let mut rng = rand::thread_rng();
-        let mut seed: Seed = [0u8; 32];
-
-        rng.fill_bytes(&mut seed);
-
-        (Self::from_seed(&seed), seed)
-    }
-
-    fn from_seed(seed: &Seed) -> Self {
-        // TODO.shelven: do better than just unwrap()
-        IdentityKey(ecdsa::Pair::from_seed_slice(seed).unwrap())
+    fn from_seed(seed: &Seed) -> Result<Self, SecretStringError> {
+        let pair = ecdsa::Pair::from_seed_slice(seed)?;
+        Ok(IdentityKey(pair))
     }
 
     fn seed(&self) -> Seed {
@@ -47,18 +37,28 @@ impl IdentityKey {
 mod test {
     use super::*;
 
+    fn generate_identity_key() -> (IdentityKey, Seed) {
+        use rand::RngCore;
+        let mut rng = rand::thread_rng();
+        let mut seed: Seed = [0u8; 32];
+
+        rng.fill_bytes(&mut seed);
+
+        (IdentityKey::from_seed(&seed).unwrap(), seed)
+    }
+
     #[test]
     fn key_generation() {
-        let (key1, seed1) = IdentityKey::generate();
-        let key2 = IdentityKey::from_seed(&seed1);
+        let (key1, seed1) = generate_identity_key();
+        let key2 = IdentityKey::from_seed(&seed1).unwrap();
 
-        assert_eq!(key1.public(), key2.public());
         assert_eq!(key1.seed(), key2.seed());
+        assert_eq!(key1.public(), key2.public());
     }
 
     #[test]
     fn sign_and_verify() {
-        let (key, _) = IdentityKey::generate();
+        let (key, _) = generate_identity_key();
         let message = [233u8; 32];
 
         let sig = key.sign(&message);
