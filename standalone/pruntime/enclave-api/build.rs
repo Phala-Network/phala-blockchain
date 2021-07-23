bae40931aae44f7ffbdea56b9ef0efc488691c03
Kevin Wang <wy721@qq.com>
2021-07-23 09:25:38 +0800

    Support proto templete rendering

--- standalone/pruntime/enclave-api/build.rs
+++ standalone/pruntime/enclave-api/build.rs
@@ -2,10 +2,19 @@ fn main() {
     #[cfg(feature = "std")]
     {
         // Path #1
+        use tera::{Template, Context, Tera};
+
+        let tmpl_str = std::fs::read_to_string("proto/pruntime_rpc.proto").unwrap();
+        let tmpl = Template::new("proto", None, &tmpl_str).unwrap();
+        let mut tera = Tera::new("proto/*").unwrap();
+        tera.templates.insert("pruntime_rpc.proto".to_owned(), tmpl);
+        let render_output = std::fs::File::create("proto/pruntime_rpc_rendered.proto").unwrap();
+        tera.render_to("pruntime_rpc.proto", &Context::new(), render_output).unwrap();
+
         prpc_build::configure()
             .out_dir("./src")
             .disable_package_emission()
-            .compile(&["proto/pruntime_rpc.proto"], &["proto"])
+            .compile(&["proto/pruntime_rpc_rendered.proto"], &["proto"])
             .unwrap();
     }
 
