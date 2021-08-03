76b9f34e3d50c64e68aeb99217c27a45986b3da0
h4x3rotab <h4x3rotab@gmail.com>
2021-08-03 19:35:33 +0000

    fix indent

--- standalone/runtime/src/msg_routing.rs
+++ standalone/runtime/src/msg_routing.rs
@@ -32,7 +32,7 @@ impl pallet_mq::QueueNotifyConfig for MessageRouteConfig {
         route_handlers! {
             PhalaRegistry::on_message_received,
             PhalaMining::on_gk_message_received,
-			PhalaMining::on_mining_message_received,
+            PhalaMining::on_mining_message_received,
             BridgeTransfer::on_message_received,
             // KittyStorage::on_message_received,
         };
