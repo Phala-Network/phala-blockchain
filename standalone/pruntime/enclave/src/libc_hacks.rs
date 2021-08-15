04dd2733d48a9e9f5eafa3302985377935a072bc
Kevin Wang <wy721@qq.com>
2021-08-15 10:17:25 +0800

    libc: Log calling not_allowed function name

--- standalone/pruntime/enclave/src/libc_hacks.rs
+++ standalone/pruntime/enclave/src/libc_hacks.rs
@@ -15,11 +15,23 @@ macro_rules! assert_eq_size {
     };
 }
 
+macro_rules! function {
+    () => {{
+        fn f() {}
+        fn type_name_of<T>(_: T) -> &'static str {
+            std::any::type_name::<T>()
+        }
+        let name = type_name_of(f);
+        &name[..name.len() - 3]
+    }};
+}
+
 macro_rules! not_allowed {
     () => {
         not_allowed!(-1)
     };
     ($rv: expr) => {{
+        error!("NOT ALLOED TO CALL {}", function!());
         sgx_libc::set_errno(libc::EPERM);
         ($rv) as _
     }};
