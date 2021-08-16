aa3d79d18acbea880c25bc5e060d1b9b0eb202e9
Shelven Zhou <favoring.zhou49@gmail.com>
2021-08-15 23:34:35 -0400

    use serialization to seal master key

--- standalone/pruntime/enclave/src/system/master_key.rs
+++ standalone/pruntime/enclave/src/system/master_key.rs
@@ -1,13 +1,24 @@
-use crate::std::io::{Read, Write};
+use crate::std::convert::TryInto;
 use crate::std::path::PathBuf;
 use crate::std::prelude::v1::*;
 use crate::std::sgxfs::SgxFile;
 
-use phala_crypto::sr25519::{Persistence, Signing, SECRET_KEY_LENGTH, SIGNATURE_BYTES};
+use phala_crypto::sr25519::{Persistence, Signature, Signing, Sr25519SecretKey};
 use sp_core::sr25519;
+
+use serde::{Deserialize, Serialize};
+use serde_bytes;
 
 /// Master key filepath
 pub const MASTER_KEY_FILE: &str = "master_key.seal";
+
+#[derive(Debug, Deserialize, Serialize)]
+struct PersistentMasterKey {
+    #[serde(with = "serde_bytes")]
+    secret_vec: Vec<u8>,
+    #[serde(with = "serde_bytes")]
+    signature_vec: Vec<u8>,
+}
 
 fn master_key_file_path(sealing_path: String) -> PathBuf {
     PathBuf::from(&sealing_path).join(MASTER_KEY_FILE)
@@ -16,18 +27,19 @@ fn master_key_file_path(sealing_path: String) -> PathBuf {
 /// Seal master key seed with signature to ensure integrity
 pub fn seal(sealing_path: String, master_key: &sr25519::Pair, identity_key: &sr25519::Pair) {
     let secret = master_key.dump_secret_key();
-    let sig = identity_key.sign_data(&secret);
+    let signature_vec = identity_key.sign_data(&secret).0.to_vec();
+    let secret_vec = secret.to_vec();
 
-    // TODO(shelven): use serialization rather than manual concat.
-    let mut buf = Vec::new();
-    buf.extend_from_slice(&secret);
-    buf.extend_from_slice(sig.as_ref());
-
+    let data = PersistentMasterKey {
+        secret_vec,
+        signature_vec,
+    };
     let filepath = master_key_file_path(sealing_path);
     info!("Seal master key to {}", filepath.as_path().display());
-    let mut file = SgxFile::create(filepath)
+
+    let file = SgxFile::create(filepath)
         .unwrap_or_else(|e| panic!("Create master key file failed: {:?}", e));
-    file.write_all(&buf)
+    serde_cbor::to_writer(file, &data)
         .unwrap_or_else(|e| panic!("Seal master key failed: {:?}", e));
 }
 
@@ -37,7 +49,7 @@ pub fn seal(sealing_path: String, master_key: &sr25519::Pair, identity_key: &sr2
 pub fn try_unseal(sealing_path: String, identity_key: &sr25519::Pair) -> Option<sr25519::Pair> {
     let filepath = master_key_file_path(sealing_path);
     info!("Unseal master key from {}", filepath.as_path().display());
-    let mut file = match SgxFile::open(filepath) {
+    let file = match SgxFile::open(filepath) {
         Ok(file) => file,
         Err(e) => {
             warn!("Failed to unseal saved master key: {:?}", e);
@@ -45,31 +57,21 @@ pub fn try_unseal(sealing_path: String, identity_key: &sr25519::Pair) -> Option<
         }
     };
 
-    let mut secret = [0_u8; SECRET_KEY_LENGTH];
-    let mut sig = [0_u8; SIGNATURE_BYTES];
+    let data: PersistentMasterKey = serde_cbor::from_reader(file)
+        .unwrap_or_else(|e| panic!("Failed to unseal saved master key: {:?}", e));
 
-    let n = file
-        .read(secret.as_mut())
-        .unwrap_or_else(|e| panic!("Read master key failed: {:?}", e));
-    if n < SECRET_KEY_LENGTH {
-        panic!(
-            "Unexpected sealed secret key length {}, expected {}",
-            n, SECRET_KEY_LENGTH
-        );
-    }
-
-    let n = file
-        .read(sig.as_mut())
-        .unwrap_or_else(|e| panic!("Read master key sig failed: {:?}", e));
-    if n < SIGNATURE_BYTES {
-        panic!(
-            "Unexpected sealed seed sig length {}, expected {}",
-            n, SIGNATURE_BYTES
-        );
-    }
+    let secret: Sr25519SecretKey = data
+        .secret_vec
+        .try_into()
+        .unwrap_or_else(|e| panic!("Unseal master key failed: {:?}", e));
+    let signature: [u8; 64] = data
+        .signature_vec
+        .try_into()
+        .unwrap_or_else(|e| panic!("Unseal signature failed: {:?}", e));
+    let signature = Signature::from_raw(signature);
 
     assert!(
-        identity_key.verify_data(&phala_crypto::sr25519::Signature::from_raw(sig), &secret),
+        identity_key.verify_data(&signature, &secret),
         "Broken sealed master key"
     );
 
