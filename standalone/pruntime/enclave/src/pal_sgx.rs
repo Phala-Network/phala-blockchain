0a7a949a6135011364e0c68ed6cafff00eb9c1a3
Kevin Wang <wy721@qq.com>
2021-09-22 10:21:23 +0800

    async enclave: Delay one second to avoid net storm

--- standalone/pruntime/enclave/src/pal_sgx.rs
+++ standalone/pruntime/enclave/src/pal_sgx.rs
@@ -54,7 +54,12 @@ impl AsyncSpawn for SgxPlatform {
     fn spawn_async(
         future: impl futures::Future<Output = ()> + Send + 'static,
     ) -> Self::Task {
-        EXECUTOR.spawn(future)
+        EXECUTOR.spawn(async {
+            use async_std::task;
+            // Delay one second to avoid net traffic storm when replaying blocks.
+            task::sleep(Duration::from_secs(1)).await;
+            future.await;
+        })
     }
 }
 
