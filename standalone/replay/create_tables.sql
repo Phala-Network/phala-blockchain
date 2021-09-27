d44c8914c743fe16bf5a33e1fa1ff1feb6902cfd
Kevin Wang <wy721@qq.com>
2021-09-27 16:57:06 +0800

    replay: SQL for TimescaleDB

--- standalone/replay/create_tables.sql
+++ standalone/replay/create_tables.sql
@@ -1,5 +1,11 @@
+-- Steps to prepare the database.
+-- 1. Install TimescaleDB.
+--   docker run -d --name timescaledb -p 5432:5432 -e POSTGRES_PASSWORD=password timescale/timescaledb:latest-pg12
+-- 2. Create a database in TimescaleDB.
+-- 3. Create table in the database with this SQL scripts.
+
 DROP TABLE IF EXISTS "worker_finance_events";
-CREATE TABLE "public"."worker_finance_events" (
+CREATE TABLE "worker_finance_events" (
     "sequence" bigint NOT NULL,
     "pubkey" bytea NOT NULL,
     "block" integer NOT NULL,
@@ -9,3 +15,10 @@ CREATE TABLE "public"."worker_finance_events" (
     "p" numeric NOT NULL,
     "payout" numeric NOT NULL
 ) WITH (oids = false);
+
+-- Doc: https://docs.timescale.com/api/latest/hypertable/create_hypertable/#optional-arguments
+SELECT create_hypertable(
+    'worker_finance_events',
+    'time',
+    chunk_time_interval := interval '7 days'
+);
