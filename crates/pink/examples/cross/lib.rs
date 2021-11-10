c78fbd075f9934d534747587632ac2b56247f0b5
Kevin Wang <wy721@qq.com>
2021-11-10 09:03:22 +0800

    Add pink examples

---
+++ crates/pink/examples/cross/lib.rs
@@ -0,0 +1,39 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+use ink_lang as ink;
+
+#[ink::contract]
+mod cross {
+    use flip::Flip;
+    use hex_literal::hex;
+
+    //--snip--
+    #[ink(storage)]
+    pub struct MyContract {
+        /// The other contract.
+        flip: Flip,
+    }
+
+    impl MyContract {
+        /// Instantiate `MyContract with the given
+        /// sub-contract codes and some initial value.
+        #[ink(constructor)]
+        pub fn new() -> Self {
+            let hash = hex!("a3f91e98edc8ccfb035946133027dd5a3f8694c70e7a27ffdf8056f7b9cc40ab").into();
+            let flip = Flip::new(true)
+                .endowment(100000)
+                .salt_bytes(&[0x00])
+                .code_hash(hash)
+                .instantiate()
+                .expect("failed at instantiating the `OtherContract` contract..");
+            Self { flip }
+        }
+
+        /// Calls the other contract.
+        #[ink(message)]
+        pub fn call_other_contract(&self) -> bool {
+            self.flip.get()
+        }
+    }
+    //--snip--
+}
