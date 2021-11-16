30ed38971096c0d08700e4d22bc3f1d6fee65db5
Kevin Wang <wy721@qq.com>
2021-11-16 12:53:05 +0800

    nit: rename

--- crates/pink/src/export_fixtures.rs
+++ crates/pink/src/export_fixtures.rs
@@ -1,4 +1,4 @@
-pub fn include_test_wasm(name: &str) -> Vec<u8> {
+pub fn load_test_wasm(name: &str) -> Vec<u8> {
     match name {
         "hooks_test" => include_bytes!("../tests/fixtures/hooks_test/hooks_test.wasm").to_vec(),
         _ => panic!("{} not found", name),
