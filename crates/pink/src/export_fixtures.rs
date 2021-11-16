a5660ae9beffee25af4d12cdb225a19be2f0ee09
Kevin Wang <wy721@qq.com>
2021-11-16 12:03:34 +0800

    pink: Add tests

---
+++ crates/pink/src/export_fixtures.rs
@@ -0,0 +1,6 @@
+pub fn include_test_wasm(name: &str) -> Vec<u8> {
+    match name {
+        "hooks_test" => include_bytes!("../tests/fixtures/hooks_test/hooks_test.wasm").to_vec(),
+        _ => panic!("{} not found", name),
+    }
+}
--- crates/pink/src/lib.rs
+++ crates/pink/src/lib.rs
@@ -1,7 +1,9 @@
 mod storage;
 mod contract;
+mod export_fixtures;
 pub mod runtime;
 
 pub mod types;
 
 pub use contract::{Contract, ContractFile, Storage};
+pub use export_fixtures::include_test_wasm;
