dd3181949b65395e04f44159f8ded7694d3b3bfb
flipchan <flipchan@users.noreply.github.com>
2022-01-06 14:28:41 +0200

    Update llvm_check.sh
    checks versions

--- ring/mk/llvm_check.sh
+++ ring/mk/llvm_check.sh
@@ -3,9 +3,7 @@
 
 function llvm_version_check() {
   tmpllvm="$(llvm-config --version)" # Check if we can get the llvm version from llvm-config
-  if [ $tmpllvm ]
-  then
+  if [ $tmpllvm > 10.0.0 ] || [ $tmpllvm > $llvm_version ]; then # Only change llvm_version if newer then version 10 or the local version is newer then the current llvm_version. 
         export llvm_version=$tmpllvm
   fi
 }
-
