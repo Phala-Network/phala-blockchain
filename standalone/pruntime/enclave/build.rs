24b99efd06187eb1327c22dfe7238f7d70abc6a4
Kevin Wang <wy721@qq.com>
2022-01-19 18:16:38 +0800

    enclave: Dont link to sgx_tprotected_fs

--- standalone/pruntime/enclave/build.rs
+++ standalone/pruntime/enclave/build.rs
@@ -1,10 +1,6 @@
 use std::env;
 
 fn main() {
-    let sdk_dir = env::var("SGX_SDK").unwrap_or_else(|_| "/opt/intel/sgxsdk".to_string());
-    println!("cargo:rustc-link-search=native={}/lib64", sdk_dir);
-    println!("cargo:rustc-link-lib=static=sgx_tprotected_fs");
-
     let ias_env = env::var("IAS_ENV").unwrap_or_else(|_| "DEV".to_string());
     match ias_env.as_ref() {
         "PROD" => {
