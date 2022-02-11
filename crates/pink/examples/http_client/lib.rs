58d5d66f6a7372bea5700f9627dd5f8b0cf680e3
Kevin Wang <wy721@qq.com>
2022-02-11 16:53:04 +0800

    mv http_get to http_client

--- crates/pink/examples/http_client/lib.rs
+++ crates/pink/examples/http_client/lib.rs
@@ -5,15 +5,15 @@ use pink_extension as pink;
 
 
 #[pink::contract(env=PinkEnvironment)]
-mod http_get {
+mod http_client {
     use super::pink;
-    use pink::{PinkEnvironment, http_get};
+    use pink::{PinkEnvironment, http_post, http_get};
     use alloc::vec::Vec;
 
     #[ink(storage)]
-    pub struct HttpGet {}
+    pub struct HttpClient {}
 
-    impl HttpGet {
+    impl HttpClient {
         #[ink(constructor)]
         pub fn default() -> Self {
             Self {}
@@ -22,6 +22,12 @@ mod http_get {
         #[ink(message)]
         pub fn get_ip(&self) -> (u16, Vec<u8>) {
             let resposne = http_get!("https://ip.kvin.wang");
+            (resposne.status_code, resposne.body)
+        }
+
+        #[ink(message)]
+        pub fn post_data(&self) -> (u16, Vec<u8>) {
+            let resposne = http_post!("https://example.com", b"payload".to_vec());
             (resposne.status_code, resposne.body)
         }
     }
