b5e97b8f3e2a7a70e6d1e50dea3a9e4041a90fd3
jasl <jasl9187@hotmail.com>
2022-03-11 07:34:06 +0800

    Disable generate node shell completion because it break testing build

--- standalone/node/build.rs
+++ standalone/node/build.rs
@@ -31,7 +31,7 @@ mod cli {
 	use substrate_build_script_utils::{generate_cargo_keys, rerun_if_git_head_changed};
 
 	pub fn main() {
-		build_shell_completion();
+		// build_shell_completion();
 		generate_cargo_keys();
 
 		rerun_if_git_head_changed();
