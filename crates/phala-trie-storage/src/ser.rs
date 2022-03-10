18508f0290309b138244316de7104878212f8d4b
Kevin Wang <wy721@qq.com>
2022-03-10 09:56:52 +0800

    pruntime: Add tests to detect block ABI changes

--- crates/phala-trie-storage/src/ser.rs
+++ crates/phala-trie-storage/src/ser.rs
@@ -1,17 +1,18 @@
 use alloc::vec::Vec;
 use serde::{Deserialize, Serialize};
 use parity_scale_codec::{Encode, Decode};
+use scale_info::TypeInfo;
 
 use super::{ChildStorageCollection, StorageCollection};
 
-#[derive(Serialize, Deserialize, Encode, Decode, Clone, Debug, Default)]
+#[derive(Serialize, Deserialize, TypeInfo, Encode, Decode, Clone, Debug, Default)]
 #[serde(rename_all = "camelCase", crate = "serde")]
 pub struct StorageChanges {
     pub main_storage_changes: StorageCollection,
     pub child_storage_changes: ChildStorageCollection,
 }
 
-#[derive(Serialize, Deserialize, Encode, Decode, Clone, Debug)]
+#[derive(Serialize, Deserialize, TypeInfo, Encode, Decode, Clone, Debug)]
 #[serde(crate = "serde")]
 pub struct StorageData {
     pub inner: Vec<(Vec<u8>, Vec<u8>)>,
