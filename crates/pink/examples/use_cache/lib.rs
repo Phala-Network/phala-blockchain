bfffa424950d99e1b1fd844cdb5e09a1b6ef8d7f
Kevin Wang <wy721@qq.com>
2022-03-15 15:49:53 +0800

    pink-example: Add test for use_cache

--- crates/pink/examples/use_cache/lib.rs
+++ crates/pink/examples/use_cache/lib.rs
@@ -26,4 +26,38 @@ mod use_cache {
             assert_eq!(ext().cache_get(b"key"), None);
         }
     }
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+        use ink_lang as ink;
+        #[ink::test]
+        fn it_works() {
+            use pink_extension::chain_extension::mock;
+            use std::{cell::RefCell, collections::HashMap, rc::Rc};
+
+            let storage: Rc<RefCell<HashMap<Vec<u8>, Vec<u8>>>> = Default::default();
+
+            {
+                let storage = storage.clone();
+                mock::mock_cache_set(move |k, v| {
+                    storage.borrow_mut().insert(k.to_vec(), v.to_vec());
+                    Ok(())
+                });
+            }
+            {
+                let storage = storage.clone();
+                mock::mock_cache_get(move |k| storage.borrow().get(k).map(|v| v.to_vec()));
+            }
+            {
+                let storage = storage.clone();
+                mock::mock_cache_remove(move |k| {
+                    storage.borrow_mut().remove(k).map(|v| v.to_vec())
+                });
+            }
+
+            let contract = UseCache::default();
+            contract.test();
+        }
+    }
 }
