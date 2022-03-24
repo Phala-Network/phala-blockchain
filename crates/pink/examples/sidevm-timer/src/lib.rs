f6f48edeb72c71e44346f33fccc12f9f0db2798c
Kevin Wang <wy721@qq.com>
2022-03-24 17:07:38 +0800

    sidevm: define ocall number

--- crates/pink/examples/sidevm-timer/src/lib.rs
+++ crates/pink/examples/sidevm-timer/src/lib.rs
@@ -2,6 +2,9 @@ extern "C" {
     fn sidevm_ocall(func_id: i32, p0: i32, p1: i32, p2: i32, p3: i32) -> i32;
 }
 
+const OCALL_CREATE_TIMER: i32 = 1;
+const OCALL_POLL_TIMER: i32 = 2;
+
 // entry point
 #[no_mangle]
 extern "C" fn sidevm_poll() -> i32 {
@@ -10,13 +13,13 @@ extern "C" fn sidevm_poll() -> i32 {
         // create a timer if it doesn't exist
         if TIMER_ID == -1 {
             let timeout = 1000 * 10; // 10 seconds
-            TIMER_ID = sidevm_ocall(1, timeout, 0, 0, 0);
+            TIMER_ID = sidevm_ocall(OCALL_CREATE_TIMER, timeout, 0, 0, 0);
             if TIMER_ID == -1 {
                 return 2;
             }
         }
 
         // poll the timer
-        sidevm_ocall(2, TIMER_ID, 0, 0, 0)
+        sidevm_ocall(OCALL_POLL_TIMER, TIMER_ID, 0, 0, 0)
     }
 }
