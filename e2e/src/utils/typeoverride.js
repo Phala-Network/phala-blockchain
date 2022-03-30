b846f11f3f2cc9fba3e89a01cea6afcc08cc00b9
Shelven Zhou <favoring.zhou49@gmail.com>
2022-03-30 11:20:20 +0000

    fix: only append enum value for parity-scale-codec compatibility

--- e2e/src/utils/typeoverride.js
+++ e2e/src/utils/typeoverride.js
@@ -62,12 +62,12 @@ const types = {
     "MessageOrigin": {
         "_enum": {
             "Pallet": "Vec<u8>",
-            "Cluster": "H256",
             "Contract": "H256",
             "Worker": "Sr25519PublicKey",
             "AccountId": "H256",
             "MultiLocation": "Vec<u8>",
-            "Gatekeeper": null
+            "Gatekeeper": null,
+            "Cluster": "H256"
         }
     },
     "Attestation": {
