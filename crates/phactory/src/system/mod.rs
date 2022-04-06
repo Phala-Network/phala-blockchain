393a8fda442aceede02f6d6e1abd04a8815e4f56
Kevin Wang <wy721@qq.com>
2022-04-06 20:58:11 +0800

    Fix matching StartSideVM

--- crates/phactory/src/system/mod.rs
+++ crates/phactory/src/system/mod.rs
@@ -1184,7 +1184,7 @@ pub fn apply_pink_side_effects(
             PinkEvent::OnBlockEndSelector(selector) => {
                 contract.set_on_block_end_selector(selector);
             }
-            PinkEvent::StartSideVM(_) => {
+            PinkEvent::StartSideVM { .. } => {
                 todo!("TODO.kevin.must: implement it")
             }
         }
