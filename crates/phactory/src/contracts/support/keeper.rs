4582697fc7630158bb42e1286073c87e3cb5e4f4
Kevin Wang <wy721@qq.com>
2022-04-12 12:00:59 +0800

    Refactor the fat contract.

--- crates/phactory/src/contracts/support/keeper.rs
+++ crates/phactory/src/contracts/support/keeper.rs
@@ -1,107 +1,102 @@
-use phala_mq::ContractId;
 use serde::{Deserialize, Serialize};
 use std::collections::BTreeMap;
-use std::ops::{Deref, DerefMut};
 
-use super::{Contract, NativeCompatContract, NativeContractWrapper};
-use crate::contracts::{
-    assets::Assets, balances::Balances, btc_lottery::BtcLottery, btc_price_bot::BtcPriceBot,
-    geolocation::Geolocation, guess_number::GuessNumber, pink::Pink,
+use crate::{
+    contracts::{
+        assets::Assets, balances::Balances, btc_lottery::BtcLottery, btc_price_bot::BtcPriceBot,
+        geolocation::Geolocation, guess_number::GuessNumber, pink::Pink, FatContract,
+        NativeContext, NativeContract as _, TransactionError, TransactionResult,
+    },
+    types::{deopaque_query, OpaqueError, OpaqueQuery, OpaqueReply},
 };
+use parity_scale_codec::{Decode, Encode};
+use phala_mq::{ContractId, MessageOrigin};
 
+use super::QueryContext;
 
-type ContractMap = BTreeMap<ContractId, AnyContract>;
-type Compat<T> = NativeCompatContract<NativeContractWrapper<T>>;
+type ContractMap = BTreeMap<ContractId, FatContract>;
 
-#[derive(Serialize, Deserialize)]
-pub enum AnyContract {
-    Pink(NativeCompatContract<Pink>),
-    Balances(Compat<Balances>),
-    Assets(Compat<Assets>),
-    BtcLottery(Compat<BtcLottery>),
-    Geolocation(Compat<Geolocation>),
-    GuessNumber(Compat<GuessNumber>),
-    BtcPriceBot(Compat<BtcPriceBot>),
-}
-
-impl Deref for AnyContract {
-    type Target = dyn Contract;
-
-    fn deref(&self) -> &Self::Target {
-        match self {
-            AnyContract::Pink(c) => c,
-            AnyContract::Balances(c) => c,
-            AnyContract::Assets(c) => c,
-            AnyContract::BtcLottery(c) => c,
-            AnyContract::Geolocation(c) => c,
-            AnyContract::GuessNumber(c) => c,
-            AnyContract::BtcPriceBot(c) => c,
+macro_rules! define_any_native_contract {
+    (pub enum $name:ident { $($contract:ident ($contract_type: tt),)* }) => {
+        #[derive(Encode, Decode)]
+        pub enum $name {
+            $($contract($contract_type),)*
         }
-    }
-}
 
-impl DerefMut for AnyContract {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        match self {
-            AnyContract::Pink(c) => c,
-            AnyContract::Balances(c) => c,
-            AnyContract::Assets(c) => c,
-            AnyContract::BtcLottery(c) => c,
-            AnyContract::Geolocation(c) => c,
-            AnyContract::GuessNumber(c) => c,
-            AnyContract::BtcPriceBot(c) => c,
+        impl $name {
+            pub(crate) fn handle_command(
+                &mut self,
+                origin: MessageOrigin,
+                cmd: Vec<u8>,
+                context: &mut NativeContext,
+            ) -> TransactionResult {
+                match self {
+                    $(Self::$contract(me) => {
+                        let cmd = Decode::decode(&mut &cmd[..]).or(Err(TransactionError::BadInput))?;
+                        me.handle_command(origin, cmd, context)
+                    })*
+                }
+            }
+
+            pub(crate) fn on_block_end(&mut self, context: &mut NativeContext) -> TransactionResult {
+                match self {
+                    $(Self::$contract(me) => {
+                        me.on_block_end(context)
+                    })*
+                }
+            }
+
+            pub(crate) fn snapshot(&self) -> Self {
+                match self {
+                    $($name::$contract(me) => {
+                        Self::$contract(me.snapshot())
+                    })*
+                }
+            }
+
+            pub(crate) fn handle_query(
+                &self,
+                origin: Option<&runtime::AccountId>,
+                req: OpaqueQuery,
+                context: &mut QueryContext,
+            ) -> Result<OpaqueReply, OpaqueError> {
+                match self {
+                    $($name::$contract(me) => {
+                        let response = me.handle_query(origin, deopaque_query(req)?, context);
+                        Ok(response.encode())
+                    })*
+                }
+            }
         }
-    }
+
+        $(
+            impl From<$contract_type> for $name {
+                fn from(c: $contract_type) -> Self {
+                    $name::$contract(c)
+                }
+            }
+        )*
+    };
 }
 
-impl From<NativeCompatContract<Pink>> for AnyContract {
-    fn from(c: NativeCompatContract<Pink>) -> Self {
-        AnyContract::Pink(c)
+define_any_native_contract!(
+    pub enum AnyContract {
+        Pink(Pink),
+        Balances(Balances),
+        Assets(Assets),
+        BtcLottery(BtcLottery),
+        Geolocation(Geolocation),
+        GuessNumber(GuessNumber),
+        BtcPriceBot(BtcPriceBot),
     }
-}
+);
 
-impl From<Compat<Balances>> for AnyContract {
-    fn from(c: Compat<Balances>) -> Self {
-        AnyContract::Balances(c)
-    }
-}
-
-impl From<Compat<Assets>> for AnyContract {
-    fn from(c: Compat<Assets>) -> Self {
-        AnyContract::Assets(c)
-    }
-}
-
-impl From<Compat<BtcLottery>> for AnyContract {
-    fn from(c: Compat<BtcLottery>) -> Self {
-        AnyContract::BtcLottery(c)
-    }
-}
-
-impl From<Compat<Geolocation>> for AnyContract {
-    fn from(c: Compat<Geolocation>) -> Self {
-        AnyContract::Geolocation(c)
-    }
-}
-
-impl From<Compat<GuessNumber>> for AnyContract {
-    fn from(c: Compat<GuessNumber>) -> Self {
-        AnyContract::GuessNumber(c)
-    }
-}
-
-impl From<Compat<BtcPriceBot>> for AnyContract {
-    fn from(c: Compat<BtcPriceBot>) -> Self {
-        AnyContract::BtcPriceBot(c)
-    }
-}
 
 #[derive(Default, Serialize, Deserialize)]
 pub struct ContractsKeeper(ContractMap);
 
 impl ContractsKeeper {
-    pub fn insert(&mut self, contract: impl Into<AnyContract>) {
-        let contract = contract.into();
+    pub fn insert(&mut self, contract: FatContract) {
         self.0.insert(contract.id(), contract);
     }
 
@@ -109,16 +104,16 @@ impl ContractsKeeper {
         self.0.keys()
     }
 
-    pub fn get_mut(&mut self, id: &ContractId) -> Option<&mut AnyContract> {
+    pub fn get_mut(&mut self, id: &ContractId) -> Option<&mut FatContract> {
         self.0.get_mut(id)
     }
 
-    pub fn get(&self, id: &ContractId) -> Option<&AnyContract> {
+    pub fn get(&self, id: &ContractId) -> Option<&FatContract> {
         self.0.get(id)
     }
 
     #[cfg(test)]
-    pub fn values_mut(&mut self) -> impl Iterator<Item = &mut AnyContract> {
+    pub fn values_mut(&mut self) -> impl Iterator<Item = &mut FatContract> {
         self.0.values_mut()
     }
 
