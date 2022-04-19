ec7ece6e110fd3b3f3a29db118657d9842874cc4
Kevin Wang <wy721@qq.com>
2022-04-19 18:43:10 +0800

    Use clap instead of structopt

--- standalone/replay/src/main.rs
+++ standalone/replay/src/main.rs
@@ -1,53 +1,54 @@
 mod replay_gk;
 
-use structopt::StructOpt;
+use clap::{AppSettings, Parser};
 
-#[derive(Debug, StructOpt)]
-#[structopt(name = "replay")]
+#[derive(Parser, Debug)]
+#[clap(about = "The Phala TEE worker app.", version, author)]
+#[clap(global_setting(AppSettings::DeriveDisplayOrder))]
 pub struct Args {
-    #[structopt(
+    #[clap(
         default_value = "ws://localhost:9944",
         long,
         help = "Substrate rpc websocket endpoint."
     )]
     node_uri: String,
 
-    #[structopt(
+    #[clap(
         default_value = "413895",
         long,
         help = "The block number to start to replay at."
     )]
     start_at: u32,
 
-    #[structopt(
+    #[clap(
         default_value = "127.0.0.1:8080",
         long,
         help = "Bind address for local HTTP server."
     )]
     bind_addr: String,
 
-    #[structopt(
+    #[clap(
         default_value = "",
         long,
         help = "The PostgresQL database to store the events."
     )]
     persist_events_to: String,
 
-    #[structopt(
+    #[clap(
         default_value = "0",
         long,
         help = "Assume the give number of block finalized."
     )]
     assume_finalized: u32,
 
-    #[structopt(
+    #[clap(
         default_value = "100000",
         long,
         help = "The number of blocks between two checkpoints. 0 for disabled"
     )]
     checkpoint_interval: u32,
 
-    #[structopt(
+    #[clap(
         long,
         help = "The checkpoint file to restore from. Default is to use the latest checkpoint."
     )]
@@ -58,6 +59,6 @@ pub struct Args {
 async fn main() {
     env_logger::init();
 
-    let args = Args::from_args();
+    let args = Args::parse();
     replay_gk::replay(args).await.expect("Failed to run replay");
 }
