825a087654a852086604e52039e5fed7d91f28c3
g302ge <g302ge@protonmail.com>
2022-04-22 02:16:26 +0800

    optimize the logic waiting for unit test

--- crates/pkvdb/src/lib.rs
+++ crates/pkvdb/src/lib.rs
@@ -1,20 +1,18 @@
-use atomic_refcell::AtomicRefCell;
 use hash_db::{AsHashDB, AsPlainDB, HashDB, HashDBRef, Hasher, PlainDB, PlainDBRef, Prefix};
+use rusty_leveldb::LdbIterator;
 use rusty_leveldb::WriteBatch;
 use rusty_leveldb::DB;
 use sp_state_machine::backend::Consolidate;
-use std::cell::RefCell;
+use std::borrow::BorrowMut;
+use std::ops::DerefMut;
 use std::path::Path;
-use std::sync::Arc;
+use std::sync::Mutex;
 
-// TODO: George should use the macro to reduce some template
-
-// TODO: George for more effective performance maybe we could use the single snapshot for queries
 pub struct Kvdb<H, T>
 where
     H: Hasher,
 {
-    leveldb: AtomicRefCell<DB>,
+    leveldb: Mutex<DB>,
     null_node_data: T,
     hashed_null_node: H::Out,
 }
@@ -25,7 +23,7 @@ where
     H: Hasher,
     T: Default + PartialEq<T> + AsRef<[u8]> + for<'a> From<&'a [u8]> + Clone + Send,
 {
-    fn consolidate(&mut self, other: Self) {
+    fn consolidate(&mut self, _other: Self) {
         unimplemented!()
     }
 }
@@ -36,18 +34,28 @@ where
     T: Default + PartialEq<T> + AsRef<[u8]> + for<'a> From<&'a [u8]> + Clone + Send + Sync,
 {
     // FIXME: should add a path to leveldb
-    pub fn with_null_node(null_key: &[u8], null_node_data: T) -> Self {
+    pub fn with_null_node(_null_key: &[u8], _null_node_data: T) -> Self {
         unimplemented!()
     }
 
-    pub fn new(path: Path) -> Self {
+    pub fn new(_path: impl AsRef<Path>) -> Self {
         // use the zero vector as the default null key and data ?
         unimplemented!()
     }
 
     pub fn purge(&mut self) {
-        // TODO: because of the inner represention so the leveldb should use the prefix scan to
-        // handle the delete which prefix is [  0x00 0x00 0x00 ]
+        // TODO:george this approach is not sound for performance 
+        if let Ok(mut db_mut) = self.leveldb.lock() {
+            let mut writebatch = WriteBatch::new();
+            if let Ok(mut iter) = DB::new_iter(db_mut.borrow_mut()) {
+                while let Some((key, val)) = iter.next() {
+                    if i32::from_be_bytes(val[0..4].try_into().unwrap()) <= 0 {
+                        writebatch.delete(&key);
+                    }
+                }
+                let _ = DB::write(db_mut.borrow_mut(), writebatch, true);
+            }
+        }
     }
 
     // TODO: if we really need the consolidate function same as the MemoryDB ?
@@ -65,9 +73,9 @@ where
     T: for<'a> From<&'a [u8]>,
 {
     fn from(v: Vec<u8>) -> Self {
-        let rc = i32::from_be_bytes(&v[0..4]);
-        let value = T::from(&v[4..]);
-        Inner(value, rc)
+        let (rc, val) = v.split_at(4);
+        let value = T::from(val);
+        Inner(value, i32::from_be_bytes(rc.try_into().unwrap()))
     }
 }
 
@@ -76,8 +84,8 @@ where
     T: AsRef<[u8]>,
 {
     fn into(self) -> Vec<u8> {
-        let mut underlying = Vec::with_capcity(self.0.as_ref().len() + 4);
-        underlying.extend_from_slice(self.1.to_be_bytes());
+        let mut underlying = Vec::with_capacity(self.0.as_ref().len() + 4);
+        underlying.extend_from_slice(&self.1.to_be_bytes());
         underlying.extend_from_slice(self.0.as_ref());
         underlying
     }
@@ -89,63 +97,55 @@ where
     T: Default + PartialEq<T> + AsRef<[u8]> + for<'a> From<&'a [u8]> + Clone + Send + Sync,
 {
     fn get(&self, key: &H::Out) -> Option<T> {
-        let db_mut = self.leveldb.borrow_mut();
-        match DB::get(db_mut.deref_mut(), key.as_ref()) {
-            Ok(v) => match v.map(Inner::<T>::from) {
-                Some((value, rc)) if rc > 0 => Some(value),
+        //FIXME:george the leveldb should panic over the lock faild 
+        if let Ok(mut db_mut) = self.leveldb.lock() {
+            match DB::get(db_mut.borrow_mut(), key.as_ref()).map(Inner::<T>::from) {
+                Some(Inner(value, rc)) if rc > 0 => Some(value),
                 _ => None,
-            },
-            _ => None,
+            }
+        } else {
+            None
         }
     }
 
     fn contains(&self, key: &H::Out) -> bool {
-        let db_mut = self.leveldb.borrow_mut();
-        match DB::get(db_mut.deref_mut(), key.as_ref()) {
-            Ok(opt) => opt.is_some(),
-            Err(_) => false,
+        let mut db_mut = self.leveldb.lock().unwrap();
+        match DB::get(db_mut.borrow_mut(), key.as_ref()).map(Inner::<T>::from) {
+            Some(Inner(_, rc)) if rc > 0 => true,
+            _ => false,
         }
     }
 
     fn emplace(&mut self, key: H::Out, value: T) {
-        let db_mut = self.leveldb.borrow_mut();
-        match DB::get(db_mut.deref_mut(), key.as_ref()) {
-            Ok(opt) => {
-                let mut writebatch = WriteBatch::new();
-                if let Some(mut inner) = opt.map(Inner::<T>::from) {
-                    inner.1 += 1;
-                    writebatch.put(key.as_ref(), inner.into::<Vec<u8>>().as_ref());
-                    let _ = DB::write(db_mut.deref_mut(), writebatch, true);
-                } else {
-                    let inner = Inner::<T>(value, 1);
-                    writebatch.put(key.as_ref(), inner.into::<Vec<u8>>().as_ref());
-                    let _ = DB::write(db_mut.deref_mut(), writebatch, true);
-                }
+        let mut db_mut = self.leveldb.lock().unwrap();
+        let mut writebatch = WriteBatch::new();
+        match DB::get(db_mut.borrow_mut(), key.as_ref()).map(Inner::<T>::from) {
+            Some(mut inner) => {
+                inner.1 += 1;
+                writebatch.put(key.as_ref(), Into::<Vec<u8>>::into(inner).as_ref());
             }
             _ => {
-                // FIXME: how to resolve the IO error over the leveldb directly panic ?
+                let inner = Inner::<T>(value, 1);
+                writebatch.put(key.as_ref(), Into::<Vec<u8>>::into(inner).as_ref());
             }
         }
+        let _ = DB::write(db_mut.borrow_mut(), writebatch, true);
     }
 
-    fn remove(&mut self, key: H::Out) {
-        let db_mut = self.leveldb.borrow_mut();
-        match DB::get(db_mut.deref_mut(), key.as_ref()) {
-            Ok(opt) => {
-                if let Some(mut inner) = opt.map(Inner::<T>::from) {
-                    if inner.1 > 1 {
-                        inner.1 -= 1;
-                        let writebatch = WriteBatch::new();
-                        writebatch.put(key.as_ref(), inner.into::<Vec<u8>>().as_ref());
-                        let _ = DB::write(db_mut.deref_mut(), writebatch, true);
-                    } else {
-                        let _ = DB::delte(db_mut.deref_mut(), key.as_ref());
-                    }
+    fn remove(&mut self, key: &H::Out) {
+        let mut db_mut = self.leveldb.lock().unwrap();
+        let mut writebatch = WriteBatch::new();
+        match DB::get(db_mut.borrow_mut(), key.as_ref()).map(Inner::<T>::from) {
+            Some(mut inner) => {
+                if inner.1 > 1 {
+                    inner.1 -= 1;
+                    writebatch.put(key.as_ref(), Into::<Vec<u8>>::into(inner).as_ref());
+                    let _ = DB::write(db_mut.borrow_mut(), writebatch, true);
+                } else {
+                    let _ = DB::delete(db_mut.borrow_mut(), key.as_ref());
                 }
             }
-            _ => {
-                // FIXME: panic too ?
-            }
+            _ => {}
         }
     }
 }
@@ -162,12 +162,9 @@ where
             return Some(self.null_node_data.clone());
         }
 
-        let db_mut = self.leveldb.borrow_mut();
-        match DB::get(db_mut.deref_mut(), key.as_ref()) {
-            Ok(v) => match v.map(Inner::<T>::from) {
-                Some((value, rc)) if rc > 0 => Some(value),
-                _ => None,
-            },
+        let mut db_mut = self.leveldb.lock().unwrap();
+        match DB::get(db_mut.borrow_mut(), key.as_ref()).map(Inner::<T>::from) {
+            Some(Inner(value, rc)) if rc > 0 => Some(value),
             _ => None,
         }
     }
@@ -177,35 +174,30 @@ where
             return true;
         }
 
-        let db_mut = self.leveldb.borrow_mut();
-        match DB::get(db_mut.deref_mut(), key.as_ref()) {
-            Ok(opt) => opt.is_some(),
-            Err(_) => false,
+        let mut db_mut = self.leveldb.lock().unwrap();
+        match DB::get(db_mut.borrow_mut(), key.as_ref()).map(Inner::<T>::from) {
+            Some(Inner(_, rc)) if rc > 0 => true,
+            _ => false,
         }
     }
 
-    fn emplace(&mut self, key: &H::Out, _prefix: Prefix, value: T) {
+    fn emplace(&mut self, key: H::Out, _prefix: Prefix, value: T) {
         if value == self.null_node_data {
             return;
         }
-        let db_mut = self.leveldb.borrow_mut();
-        match DB::get(db_mut.deref_mut(), key.as_ref()) {
-            Ok(opt) => {
-                let mut writebatch = WriteBatch::new();
-                if let Some(mut inner) = opt.map(Inner::<T>::from) {
-                    inner.1 += 1;
-                    writebatch.put(key.as_ref(), inner.into::<Vec<u8>>().as_ref());
-                    let _ = DB::write(db_mut.deref_mut(), writebatch, true);
-                } else {
-                    let inner = Inner::<T>(value, 1);
-                    writebatch.put(key.as_ref(), inner.into::<Vec<u8>>().as_ref());
-                    let _ = DB::write(db_mut.deref_mut(), writebatch, true);
-                }
+        let mut db_mut = self.leveldb.lock().unwrap();
+        let mut writebatch = WriteBatch::new();
+        match DB::get(db_mut.borrow_mut(), key.as_ref()).map(Inner::<T>::from) {
+            Some(mut inner) => {
+                inner.1 += 1;
+                writebatch.put(key.as_ref(), Into::<Vec<u8>>::into(inner).as_ref());
             }
             _ => {
-                // FIXME: how to resolve the IO error over the leveldb directly panic ?
+                let inner = Inner::<T>(value, 1);
+                writebatch.put(key.as_ref(), Into::<Vec<u8>>::into(inner).as_ref());
             }
         }
+        let _ = DB::write(db_mut.borrow_mut(), writebatch, true);
     }
 
     fn insert(&mut self, prefix: Prefix, value: &[u8]) -> H::Out {
@@ -222,28 +214,24 @@ where
             return;
         }
 
-        let db_mut = self.leveldb.borrow_mut();
-        match DB::get(db_mut.deref_mut(), key.as_ref()) {
-            Ok(opt) => {
-                if let Some(mut inner) = opt.map(Inner::<T>::from) {
-                    if inner.1 > 1 {
-                        inner.1 -= 1;
-                        let writebatch = WriteBatch::new();
-                        writebatch.put(key.as_ref(), inner.into::<Vec<u8>>().as_ref());
-                        let _ = DB::write(db_mut.deref_mut(), writebatch, true);
-                    } else {
-                        let _ = DB::delte(db_mut.deref_mut(), key.as_ref());
-                    }
+        let mut db_mut = self.leveldb.lock().unwrap();
+        match DB::get(db_mut.borrow_mut(), key.as_ref()).map(Inner::<T>::from) {
+            Some(mut inner) => {
+                if inner.1 > 1 {
+                    inner.1 -= 1;
+                    let mut writebatch = WriteBatch::new();
+                    writebatch.put(key.as_ref(), Into::<Vec<u8>>::into(inner).as_ref());
+                    let _ = DB::write(db_mut.deref_mut(), writebatch, true);
+                } else {
+                    let _ = DB::delete(db_mut.deref_mut(), key.as_ref());
                 }
             }
-            _ => {
-                // FIXME: panic too ?
-            }
+            _ => {}
         }
     }
 }
 
-impl<H, T> PlainDBRef<H, T> for Kvdb<H, T>
+impl<H, T> PlainDBRef<H::Out, T> for Kvdb<H, T>
 where
     H: Hasher,
     T: Default + PartialEq<T> + AsRef<[u8]> + for<'a> From<&'a [u8]> + Clone + Send + Sync,
@@ -274,7 +262,7 @@ where
 impl<H, T> AsPlainDB<H::Out, T> for Kvdb<H, T>
 where
     H: Hasher,
-    T: Default + PartialEq<T> + for<'a> From<&'a [u8]> + Clone + Send + Sync,
+    T: Default + PartialEq<T> + AsRef<[u8]> + for<'a> From<&'a [u8]> + Clone + Send + Sync,
 {
     fn as_plain_db(&self) -> &dyn PlainDB<H::Out, T> {
         self
@@ -301,6 +289,10 @@ where
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+    use hash_db::Hasher;
+    use sp_core::KeccakHasher; 
+
     #[test]
     fn it_works() {
         let result = 2 + 2;
