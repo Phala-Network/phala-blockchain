7bacf9ba58499cc3719ba4abfb1838ccf7981e2d
jasl <jasl9187@hotmail.com>
2022-04-26 17:34:20 +0800

    replay sql: update create sql

--- standalone/replay/create_tables.sql
+++ standalone/replay/create_tables.sql
@@ -8,14 +8,15 @@ CREATE EXTENSION IF NOT EXISTS timescaledb;
 
 DROP TABLE IF EXISTS "worker_finance_events";
 CREATE TABLE "worker_finance_events" (
-    "sequence" bigint NOT NULL UNIQUE,
+    "sequence" bigint NOT NULL,
     "pubkey" bytea NOT NULL,
     "block" integer NOT NULL,
     "time" timestamp without time zone NOT NULL,
     "event" text NOT NULL,
     "v" numeric NOT NULL,
     "p" numeric NOT NULL,
-    "payout" numeric NOT NULL
+    "payout" numeric NOT NULL,
+    PRIMARY KEY(time, sequence)
 ) WITH (oids = false);
 
 -- Doc: https://docs.timescale.com/api/latest/hypertable/create_hypertable/#optional-arguments
