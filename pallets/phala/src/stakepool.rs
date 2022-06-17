41e13af1f2d381843f88500b0959754ee9406731
jasl <jasl9187@hotmail.com>
2022-06-17 17:47:55 +0800

    chore: fmt

--- pallets/phala/src/stakepool.rs
+++ pallets/phala/src/stakepool.rs
@@ -849,7 +849,7 @@ pub mod pallet {
 			let who = ensure_signed(origin)?;
 			let mut pool_info = Self::ensure_pool(pid)?;
 			let a = amount; // Alias to reduce confusion in the code below
-				// If the pool has a contribution whitelist in storages, check if the origin is authorized to contribute
+			// If the pool has a contribution whitelist in storages, check if the origin is authorized to contribute
 			if let Some(whitelist) = PoolContributionWhitelists::<T>::get(&pid) {
 				ensure!(
 					whitelist.contains(&who) || pool_info.owner == who,
