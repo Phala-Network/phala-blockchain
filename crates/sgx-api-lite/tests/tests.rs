db4952fa1d38c8e9dec479c26f7cfd17d23d4e2d
Kevin Wang <wy721@qq.com>
2022-07-13 18:05:42 +0800

    Implement sgx-api-lite

---
+++ crates/sgx-api-lite/tests/tests.rs
@@ -0,0 +1,20 @@
+use sgx_api_lite as sgx;
+
+#[test]
+#[ignore = "only works under sgx"]
+fn it_works() {
+    // In Enclave A
+    let my_target_info = sgx::target_info().unwrap();
+    let target_info_bytes = sgx::encode(&my_target_info);
+
+    // In Enclave B
+    let its_target_info = unsafe { sgx::decode(target_info_bytes).unwrap() };
+    let report = sgx::report(&its_target_info, &[0; 64]).unwrap();
+    let report_bytes = sgx::encode(&report);
+
+    // In Enclave A
+    let recv_report = unsafe { sgx::decode(report_bytes).unwrap() };
+    let rv = sgx::verify(recv_report);
+    assert!(rv.is_ok());
+    println!("It works");
+}
