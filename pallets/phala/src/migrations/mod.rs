98f81406a5d44a8ae53ba1f80c9ee6a79dd0506a
jasl <jasl9187@hotmail.com>
2022-11-02 01:37:42 +0800

    Phala Registry pallet migration

--- pallets/phala/src/migrations/mod.rs
+++ pallets/phala/src/migrations/mod.rs
@@ -6,6 +6,7 @@ use frame_support::{
 #[allow(unused_imports)]
 use log;
 
+use sp_std::prelude::*;
 use crate::*;
 
 #[allow(dead_code)]
@@ -61,4 +62,102 @@ fn set_unified_version<T: PhalaPallets>(version: u16) {
 	StorageVersion::new(version).put::<registry::Pallet<T>>();
 	StorageVersion::new(version).put::<stakepool::Pallet<T>>();
 	StorageVersion::new(version).put::<fat_tokenomic::Pallet<T>>();
+}
+
+pub mod registry_v6_migration {
+	use codec::{Decode, Encode};
+	use scale_info::TypeInfo;
+
+	use phala_types::{WorkerPublicKey, EcdhPublicKey, AttestationProvider};
+	use crate::registry::WorkerInfo;
+	use super::*;
+
+	#[derive(Encode, Decode, TypeInfo, Debug, Clone)]
+	pub struct OldWorkerInfo<AccountId> {
+		/// The identity public key of the worker
+		pub pubkey: WorkerPublicKey,
+		/// The public key for ECDH communication
+		pub ecdh_pubkey: EcdhPublicKey,
+		/// The pruntime version of the worker upon registering
+		pub runtime_version: u32,
+		/// The unix timestamp of the last updated time
+		pub last_updated: u64,
+		/// The stake pool owner that can control this worker
+		///
+		/// When initializing pruntime, the user can specify an _operator account_. Then this field
+		/// will be updated when the worker is being registered on the blockchain. Once it's set,
+		/// the worker can only be added to a stake pool if the pool owner is the same as the
+		/// operator. It ensures only the trusted person can control the worker.
+		pub operator: Option<AccountId>,
+		/// The [confidence level](https://wiki.phala.network/en-us/mine/solo/1-2-confidential-level-evaluation/#confidence-level-of-a-miner)
+		/// of the worker
+		pub confidence_level: u8,
+		/// The performance score by benchmark
+		///
+		/// When a worker is registered, this field is set to `None`, indicating the worker is
+		/// requested to run a benchmark. The benchmark lasts [`BenchmarkDuration`] blocks, and
+		/// this field will be updated with the score once it finishes.
+		///
+		/// The `initial_score` is used as the baseline for mining performance test.
+		pub initial_score: Option<u32>,
+		/// Deprecated
+		pub features: Vec<u32>,
+	}
+
+	#[cfg(feature = "try-runtime")]
+	pub fn pre_migrate<T: PhalaPallets>() -> Result<(), &'static str> {
+		frame_support::ensure!(
+			get_versions::<T>() == unified_versions::<T>(5),
+			"incorrect pallet versions"
+		);
+		Ok(())
+	}
+
+	pub fn migrate<T>() -> Weight
+		where
+			T: PhalaPallets + frame_system::Config
+	{
+		if get_versions::<T>() != unified_versions::<T>(5) {
+			log::info!("No need to run the migration");
+			return T::DbWeight::get().reads(1)
+		}
+
+		let mut count = 0;
+		registry::pallet::Workers::<T>::translate(
+			|
+				_pubkey: WorkerPublicKey,
+				old: OldWorkerInfo<<T as frame_system::Config>::AccountId>
+			| {
+				count += 1;
+
+				Some(
+					WorkerInfo {
+						pubkey: old.pubkey,
+						ecdh_pubkey: old.ecdh_pubkey,
+						runtime_version: old.runtime_version,
+						last_updated: old.last_updated,
+						operator: old.operator,
+						attestation_provider: Some(AttestationProvider::Ias),
+						confidence_level: old.confidence_level,
+						initial_score: old.initial_score,
+						features: old.features
+					}
+				)
+			}
+		);
+
+		set_unified_version::<T>(6);
+
+		T::DbWeight::get().reads_writes(count, count)
+	}
+
+	#[cfg(feature = "try-runtime")]
+	pub fn post_migrate<T: PhalaPallets>() -> Result<(), &'static str> {
+		frame_support::ensure!(
+			get_versions::<T>() == unified_versions::<T>(6),
+			"incorrect pallet versions postmigrate"
+		);
+		log::info!("Ᵽ phala pallet migration passes POST migrate checks ✅",);
+		Ok(())
+	}
 }
