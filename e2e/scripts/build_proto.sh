2d71ac323f3131d418421d25736b6a3408d81351
Leechael Yim <yanleech@gmail.com>
2023-01-30 22:31:08 +0800

    fix the e2e script dependencies.

--- e2e/scripts/build_proto.sh
+++ e2e/scripts/build_proto.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 function buildProto() {
-  pbjs -w commonjs -t static-module "../crates/phactory/api/proto/$1.proto" -o "src/proto/$1.js"
-  pbts -o "src/proto/$1.d.ts" "src/proto/$1.js"
+  npx pbjs -w commonjs -t static-module "../crates/phactory/api/proto/$1.proto" -o "src/proto/$1.js"
+  npx pbts -o "src/proto/$1.d.ts" "src/proto/$1.js"
 }
 
 buildProto pruntime_rpc
