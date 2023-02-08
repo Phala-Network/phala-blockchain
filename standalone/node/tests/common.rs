64fecdd9acc7bad4600436175a211b5a0513dc27
jasl <jasl9187@hotmail.com>
2023-02-09 05:36:55 +0800

    update test

--- standalone/node/tests/common.rs
+++ standalone/node/tests/common.rs
@@ -167,10 +167,10 @@ pub fn find_ws_url_from_output(read: impl Read + Send) -> (String, String) {
 			// does the line contain our port (we expect this specific output from substrate).
 			let sock_addr = match line.split_once("Running JSON-RPC WS server: addr=") {
 				None => return None,
-				Some((_, after)) => after.split_once(',').unwrap().0,
+				Some((_, after)) => after.split_once(",").unwrap().0,
 			};
 
-			Some(format!("ws://{sock_addr}"))
+			Some(format!("ws://{}", sock_addr))
 		})
 		.unwrap_or_else(|| {
 			eprintln!("Observed node output:\n{}", data);
