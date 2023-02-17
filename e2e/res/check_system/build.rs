fe3b4d8491d28fa344712ebe8530487edd822c48
Kevin Wang <wy721@qq.com>
2023-02-17 10:19:55 +0000

    contracts: Remove --release && fix e2e

--- e2e/res/check_system/build.rs
+++ e2e/res/check_system/build.rs
@@ -1,4 +1,5 @@
 fn main() {
+    println!("cargo:rerun-if-changed=sideprog.wasm");
     let hash = sp_core::blake2_256(include_bytes!("./sideprog.wasm"));
     std::fs::write("./sideprog.wasm.hash", hash).unwrap();
 }
