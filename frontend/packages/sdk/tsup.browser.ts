a08727fb355ad804269af3b1dab3eb927bf16452
vabaly <love0and0life@gmail.com>
2023-03-15 20:44:59 +0800

    fix: use fetch to replace undici in browser.

--- frontend/packages/sdk/tsup.browser.ts
+++ frontend/packages/sdk/tsup.browser.ts
@@ -2,6 +2,40 @@ import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfil
 import { NodeModulesPolyfillPlugin } from "@esbuild-plugins/node-modules-polyfill";
 // import nodePolyfills from 'rollup-plugin-node-polyfills';
 import { defineConfig } from "tsup";
+import { Plugin, Loader } from 'esbuild'
+
+const ReplaceModulesPlugin = (modules: {
+  name: string
+  contents: string
+  loader?: Loader
+}[]): Plugin => {
+  return {
+    name: 'replaceModules',
+    setup(build) {
+
+      modules.forEach(moduleItem => {
+        const { name, contents, loader = 'js' } = moduleItem
+        const filter = new RegExp(`^${name}\\/?(.+)?`)
+
+        build.onResolve({ filter }, args => {
+          console.log('args', args)
+          return {
+            path: args.path,
+            namespace: name,
+          }
+        })
+
+        build.onLoad({ filter, namespace: name }, () => {
+          console.log('namespace', name)
+          return {
+            contents,
+            loader,
+          }
+        })
+      })
+    },
+  }
+}
 
 export default defineConfig({
   esbuildPlugins: [
@@ -9,6 +43,10 @@ export default defineConfig({
     NodeGlobalsPolyfillPlugin({
       buffer: true,
     }),
+    ReplaceModulesPlugin([{
+      name: 'undici',
+      contents: 'export const fetch = window.fetch;'
+    }])
   ],
   entry: ["src/index.ts"],
   outDir: "./dist/browser",
@@ -18,5 +56,5 @@ export default defineConfig({
   target: "node16",
   clean: true,
   platform: "browser",
-  noExternal: ["crypto-browserify", "protobufjs", "randomBytes"],
+  noExternal: ["crypto-browserify", "protobufjs", "randomBytes", "undici"],
 });
