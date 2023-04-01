6b867ca93c18dae2b2aa9087599c40aeb9580770
Leechael Yim <yanleech@gmail.com>
2023-04-01 18:15:22 +0800

    feat: PinkCodePromise

---
+++ frontend/packages/sdk/src/contracts/PinkCode.ts
@@ -0,0 +1,123 @@
+import type { SubmittableExtrinsic } from '@polkadot/api/submittable/types';
+import type { bool, Result } from '@polkadot/types';
+import type { ApiTypes, DecorateMethod } from '@polkadot/api/types';
+import type { AccountId, EventRecord } from '@polkadot/types/interfaces';
+import type { ISubmittableResult } from '@polkadot/types/types';
+import type { Codec } from '@polkadot/types-codec/types';
+import type { KeyringPair } from '@polkadot/keyring/types';
+// import type { AbiConstructor, BlueprintOptions } from '../types.js';
+import type { AbiConstructor, BlueprintOptions } from '@polkadot/api-contract/types';
+import type { MapConstructorExec } from '@polkadot/api-contract/base/types';
+
+import type { OnChainRegistry } from '../OnChainRegistry';
+import type { AbiLike, WasmLike } from '../types';
+
+import { SubmittableResult } from '@polkadot/api';
+import { ApiBase } from '@polkadot/api/base';
+import { BN_ZERO, compactAddLength, isUndefined, isWasm, u8aToU8a } from '@polkadot/util';
+import { createBluePrintTx } from '@polkadot/api-contract/base/util';
+
+import { Abi } from '@polkadot/api-contract/Abi';
+import { toPromiseMethod } from '@polkadot/api';
+
+export class InkCodeSubmittableResult<ApiType extends ApiTypes> extends SubmittableResult {
+  readonly registry: OnChainRegistry;
+  readonly abi: Abi;
+  // readonly blueprint?: Blueprint<ApiType>;
+  // readonly contract?: Contract<ApiType>;
+  
+  #isReady: boolean = false;
+
+  constructor (result: ISubmittableResult, abi: Abi, registry: OnChainRegistry) {
+  // constructor (result: ISubmittableResult, blueprint?: Blueprint<ApiType>, contract?: Contract<ApiType>) {
+    super(result);
+
+    this.registry = registry;
+    this.abi = abi;
+
+  //   this.blueprint = blueprint;
+  //   this.contract = contract;
+  }
+
+  async waitReady(pair: KeyringPair, timeout: number = 10_000) {
+    if (this.#isReady) {
+      return
+    }
+    if (this.isInBlock || this.isFinalized) {
+      const system = this.registry.systemContract!;
+      const codeHash = this.abi.info.source.wasmHash.toString();
+
+      const t0 = new Date().getTime();
+      while (true) {
+        const { output } = await system.query['system::codeExists'](pair, codeHash, 'Ink')
+        if (output && (output as Result<bool, any>).asOk.toPrimitive()) {
+          this.#isReady = true;
+          return
+        }
+        const t1 = new Date().getTime();
+        if (t1 - t0 > timeout) {
+          throw new Error('Timeout')
+        }
+        await new Promise(resolve => setTimeout(resolve, 500));
+      }
+    }
+    throw new Error('Not in block, your Code may upload failed.')
+  }
+}
+
+export class PinkCodePromise {
+
+  readonly abi: Abi;
+  readonly api: ApiBase<'promise'>;
+  readonly phatRegistry: OnChainRegistry;
+
+  protected readonly _decorateMethod: DecorateMethod<'promise'>;
+
+  readonly code: Uint8Array;
+
+  readonly #tx: MapConstructorExec<'promise'> = {};
+
+  constructor (api: ApiBase<'promise'>, phatRegistry: OnChainRegistry, abi: AbiLike, wasm: WasmLike) {
+    if (!api || !api.isConnected || !api.tx) {
+      throw new Error('Your API has not been initialized correctly and is not connected to a chain');
+    }
+    if (!phatRegistry.isReady()) {
+      throw new Error('Your phatRegistry has not been initialized correctly.');
+    }
+
+    this.abi = abi instanceof Abi
+      ? abi
+      : new Abi(abi, api.registry.getChainProperties());
+    this.api = api;
+    this._decorateMethod = toPromiseMethod;
+    this.phatRegistry = phatRegistry
+
+    this.code = isWasm(this.abi.info.source.wasm)
+      ? this.abi.info.source.wasm
+      : u8aToU8a(wasm);
+
+    if (!isWasm(this.code)) {
+      throw new Error('No WASM code provided');
+    }
+
+    this.abi.constructors.forEach((c): void => {
+      if (isUndefined(this.#tx[c.method])) {
+        this.#tx[c.method] = createBluePrintTx(c, (_o, p) => this.#instantiate(c, p));
+      }
+    });
+  }
+
+  public get tx (): MapConstructorExec<'promise'> {
+    return this.#tx;
+  }
+
+  #instantiate = (_constructorOrId: AbiConstructor | string | number, _params: unknown[]) => {
+    return this.api.tx.phalaPhatContracts.clusterUploadResource(
+      this.phatRegistry.clusterId,
+      'InkCode',
+      this.code
+    ).withResultTransform(
+      (result: ISubmittableResult) => new InkCodeSubmittableResult(result, this.abi, this.phatRegistry)
+    );
+  };
+}
