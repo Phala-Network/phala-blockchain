7ac0a1e1423dc648a999be6d63416142f6833189
h4x3rotab <h4x3rotab@gmail.com>
2023-04-03 07:19:23 +0000

    enable bounty council origin

--- standalone/runtime/src/lib.rs
+++ standalone/runtime/src/lib.rs
@@ -982,8 +982,8 @@ impl pallet_membership::Config<pallet_membership::Instance1> for Runtime {
 parameter_types! {
     pub const ProposalBond: Permill = Permill::from_percent(5);
     pub const ProposalBondMinimum: Balance = 1 * DOLLARS;
-    pub const SpendPeriod: BlockNumber = 1 * DAYS;
-    pub const Burn: Permill = Permill::from_percent(50);
+    pub const SpendPeriod: BlockNumber = 2 * MINUTES;
+    pub const Burn: Permill = Permill::from_percent(1);
     pub const TipCountdown: BlockNumber = 1 * DAYS;
     pub const TipFindersFee: Percent = Percent::from_percent(20);
     pub const TipReportDepositBase: Balance = 1 * DOLLARS;
@@ -1016,7 +1016,13 @@ impl pallet_treasury::Config for Runtime {
     type SpendFunds = Bounties;
     type WeightInfo = pallet_treasury::weights::SubstrateWeight<Runtime>;
     type MaxApprovals = MaxApprovals;
-    type SpendOrigin = frame_system::EnsureWithSuccess<EnsureRoot<AccountId>, AccountId, MaxBalance>;
+    type SpendOrigin = frame_system::EnsureWithSuccess<
+        frame_support::traits::EitherOf<
+            EnsureRoot<AccountId>,
+            pallet_collective::EnsureProportionMoreThan<AccountId, CouncilCollective, 1, 2>,
+        >,
+        AccountId, MaxBalance,
+    >;
 }
 
 parameter_types! {
@@ -1026,7 +1032,7 @@ parameter_types! {
     pub const CuratorDepositMultiplier: Permill = Permill::from_percent(50);
     pub const CuratorDepositMin: Balance = 1 * DOLLARS;
     pub const CuratorDepositMax: Balance = 100 * DOLLARS;
-    pub const BountyDepositPayoutDelay: BlockNumber = 1 * DAYS;
+    pub const BountyDepositPayoutDelay: BlockNumber = 2 * MINUTES;
     pub const BountyUpdatePeriod: BlockNumber = 14 * DAYS;
 }
 
