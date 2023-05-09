d1591b592357d3a96399bafb3aebad45508c7575
Kevin Wang <wy721@qq.com>
2023-05-09 10:57:46 +0000

    pruntime: Use RocksDB as contract storage

--- crates/pink/runner/src/storage.rs
+++ crates/pink/runner/src/storage.rs
@@ -1,11 +1,20 @@
-use im::OrdMap;
+use phala_trie_storage::RocksDB;
 use pink_capi::{types::Hash, v1::ocall::StorageChanges};
 use serde::{Deserialize, Serialize};
 
-#[derive(Clone, Default, Serialize, Deserialize)]
+#[derive(Default, Serialize, Deserialize)]
 pub struct ClusterStorage {
     root: Option<Hash>,
-    kv_store: OrdMap<Vec<u8>, (i32, Vec<u8>)>,
+    kv_store: RocksDB,
+}
+
+impl Clone for ClusterStorage {
+    fn clone(&self) -> Self {
+        Self {
+            root: self.root,
+            kv_store: self.kv_store.snapshot(),
+        }
+    }
 }
 
 impl ClusterStorage {
@@ -17,34 +26,16 @@ impl ClusterStorage {
         self.root = Some(root);
     }
 
-    pub fn get(&self, key: &[u8]) -> Option<&(i32, Vec<u8>)> {
-        self.kv_store.get(key)
-    }
-
-    fn update(&mut self, key: Vec<u8>, value: Vec<u8>, rc: i32) {
-        if rc == 0 {
-            return;
-        }
-        match self.kv_store.get_mut(&key) {
-            Some((ref mut old_rc, ref mut old_value)) => {
-                *old_rc += rc;
-                if rc > 0 {
-                    *old_value = value;
-                }
-                if *old_rc == 0 {
-                    self.kv_store.remove(&key);
-                }
-            }
-            None => {
-                self.kv_store.insert(key, (rc, value));
-            }
-        }
+    pub fn get(&self, key: &[u8]) -> Option<(i32, Vec<u8>)> {
+        let (value, rc) = self
+            .kv_store
+            .get_r(key)
+            .expect("Failed to get key from RocksDB")?;
+        Some((rc, value))
     }
 
     pub fn commit(&mut self, root: Hash, changes: StorageChanges) {
-        for (key, (value, rc)) in changes {
-            self.update(key, value, rc);
-        }
+        self.kv_store.consolidate(changes.into_iter());
         self.set_root(root);
     }
 }
