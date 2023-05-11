3a93ab9739b4e24853a2c0ae84ccefe1f68f919f
Kevin Wang <wy721@qq.com>
2023-05-11 14:19:41 +0800

    Test for rocksdb backend

--- crates/phala-trie-storage/src/kvdb/database.rs
+++ crates/phala-trie-storage/src/kvdb/database.rs
@@ -25,7 +25,7 @@ pub enum RocksDB {
 
 impl RocksDB {
     pub fn new() -> Self {
-        let (db, sn) = new_db();
+        let (db, sn) = create_db();
         Self::Database {
             db: Arc::new(db),
             sn,
@@ -41,8 +41,8 @@ impl RocksDB {
 
     pub fn consolidate<K: AsRef<[u8]>>(&self, other: impl Iterator<Item = (K, (Vec<u8>, i32))>) {
         let RocksDB::Database { db, .. } = self else {
-        panic!("Consolidate on a snapshot")
-    };
+            panic!("Consolidate on a snapshot")
+        };
 
         let transaction = db.transaction();
         for (key, (value, rc)) in other {
@@ -80,21 +80,23 @@ impl RocksDB {
         transaction.commit().expect("Failed to commit transaction");
     }
 
-    pub fn put(&self, key: &[u8], value: &[u8]) -> Result<(), DBError> {
+    #[cfg(test)]
+    pub(crate) fn put(&self, key: &[u8], value: &[u8]) -> Result<(), DBError> {
         let Self::Database { db, .. } = self else {
-        panic!("Put on a snapshot")
-    };
+            panic!("Put on a snapshot")
+        };
         Ok(db.put(key, value)?)
     }
 
-    pub fn delete(&self, key: &[u8]) -> Result<(), DBError> {
+    #[cfg(test)]
+    pub(crate) fn delete(&self, key: &[u8]) -> Result<(), DBError> {
         let Self::Database { db, .. } = self else {
-        panic!("Delete on a snapshot")
-    };
+            panic!("Delete on a snapshot")
+        };
         Ok(db.delete(key)?)
     }
 
-    pub fn get(&self, key: &[u8]) -> Result<Option<Vec<u8>>, DBError> {
+    fn get(&self, key: &[u8]) -> Result<Option<Vec<u8>>, DBError> {
         match self {
             RocksDB::Database { db, .. } => db.get(key),
             RocksDB::Snapshot(snap) => snap.get(key),
@@ -115,23 +117,36 @@ impl Default for RocksDB {
     }
 }
 
-pub(crate) fn new_db() -> (TransactionDB<MultiThreaded>, usize) {
-    let _todo = "Use env or take from arg";
-    const CACHE_PATH: &str = "data/protected_files/caches";
+/// The global cache dir for unit tests.
+environmental::environmental!(test_cached_path: String);
+
+#[cfg(test)]
+pub(crate) fn with_cache_dir<T>(cache_dir: &str, f: impl FnOnce() -> T) -> T {
+    let mut cache_dir = cache_dir.to_string();
+    test_cached_path::using(&mut cache_dir, f)
+}
+
+pub(crate) fn create_db() -> (TransactionDB<MultiThreaded>, usize) {
+    let test_path = test_cached_path::with(|path| path.clone());
+    if test_path.is_none() {
+        panic!("empty test path");
+    }
+    let cache_path = &test_path
+        .or_else(|| std::env::var("PHALA_TRIE_CACHE_PATH").ok())
+        .unwrap_or_else(|| "data/protected_files/caches".to_string());
     static NEXT_SN: AtomicUsize = AtomicUsize::new(0);
     let sn = NEXT_SN.fetch_add(1, Ordering::SeqCst);
     if sn == 0 {
-        if std::path::Path::new(CACHE_PATH).exists() {
-            info!("Removing cache folder: {}", CACHE_PATH);
-            std::fs::remove_dir_all(CACHE_PATH).expect("Failed to remove cache folder");
+        if std::path::Path::new(cache_path).exists() {
+            info!("Removing cache folder: {}", cache_path);
+            std::fs::remove_dir_all(cache_path).expect("Failed to remove cache folder");
         }
-        std::fs::create_dir_all(CACHE_PATH).expect("Failed to create cache folder");
     }
     let mut options = Options::default();
     options.set_max_open_files(256);
     options.create_if_missing(true);
     options.set_error_if_exists(true);
-    let path = format!("{CACHE_PATH}/cache_{sn}",);
+    let path = format!("{cache_path}/cache_{sn}",);
     let db = TransactionDB::open(&options, &Default::default(), &path).expect("Faile to open KVDB");
     (db, sn)
 }
@@ -176,9 +191,9 @@ impl<'de> Deserialize<'de> for RocksDB {
             where
                 A: MapAccess<'de>,
             {
-                let (db, sn) = new_db();
+                let (db, sn) = create_db();
                 let transaction = db.transaction();
-                while let Some((key, (value, rc))) = seq.next_entry::<Vec<u8>, (Vec<u8>, i32)>()? {
+                while let Some((key, (rc, value))) = seq.next_entry::<Vec<u8>, (i32, Vec<u8>)>()? {
                     transaction
                         .put(&key, (value, rc).encode())
                         .expect("Failed to put key in transaction");
@@ -206,4 +221,31 @@ fn decode_value<E: Display>(
             Ok(Some((d, rc)))
         }
     }
+}
+
+#[test]
+fn serde_works() {
+    let cache_dir = tempfile::tempdir().unwrap();
+    with_cache_dir(cache_dir.path().to_str().unwrap(), || {
+        let db = RocksDB::new();
+        db.put(b"foo", &(vec![42u8], 1i32).encode()).unwrap();
+        let ser = serde_cbor::to_vec(&db).unwrap();
+        let de: RocksDB = serde_cbor::from_slice(&ser).unwrap();
+        assert_eq!(de.get_r(b"foo").unwrap(), Some((vec![42], 1)));
+    });
+}
+
+#[test]
+fn snapshot_works() {
+    let cache_dir = tempfile::tempdir().unwrap();
+    with_cache_dir(cache_dir.path().to_str().unwrap(), || {
+        let db = RocksDB::new();
+        db.put(b"foo", b"bar").unwrap();
+        assert_eq!(db.get(b"foo").unwrap().unwrap(), b"bar");
+        let snapshot = db.snapshot();
+        assert_eq!(snapshot.get(b"foo").unwrap().unwrap(), b"bar");
+        db.delete(b"foo").unwrap();
+        assert_eq!(db.get(b"foo").unwrap(), None);
+        assert_eq!(snapshot.get(b"foo").unwrap().unwrap(), b"bar");
+    });
 }
