8bf7557b51b0bfb0ac33ba33378dd5bab0d16ad4
Kevin Wang <wy721@qq.com>
2023-05-25 07:50:23 +0000

    Remove the unwrap()

--- crates/pink/pink-extension-runtime/src/lib.rs
+++ crates/pink/pink-extension-runtime/src/lib.rs
@@ -46,7 +46,9 @@ impl<'a, T, E> DefaultPinkExtension<'a, T, E> {
 fn block_on<F: core::future::Future>(f: F) -> F::Output {
     match tokio::runtime::Handle::try_current() {
         Ok(handle) => handle.block_on(f),
-        Err(_) => tokio::runtime::Runtime::new().unwrap().block_on(f),
+        Err(_) => tokio::runtime::Runtime::new()
+            .expect("Failed to create tokio runtime")
+            .block_on(f),
     }
 }
 
