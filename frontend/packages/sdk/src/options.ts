d8ee4d471589ff5747ad451f0619b4152af065a4
Leechael Yim <yanleech@gmail.com>
2023-08-01 04:29:37 +0800

    refactor(jssdk): export PhalaRegistryTypes

--- frontend/packages/sdk/src/options.ts
+++ frontend/packages/sdk/src/options.ts
@@ -1,15 +1,18 @@
 import type { ApiOptions } from "@polkadot/api/types";
+import type { RegistryTypes } from "@polkadot/types/types";
 
 import { typeDefinitions } from '@polkadot/types';
 import { types } from './lib/types';
+
+export const phalaRegistryTypes = { ...types, ...typeDefinitions } as unknown as RegistryTypes
 
 export function options(options: ApiOptions = {}): ApiOptions {
   return {
     ...options,
     types: {
-      ...types,
-      ...typeDefinitions,
+      ...phalaRegistryTypes,
       ...options.types || {},
     } as unknown as ApiOptions['types'],
   }
 }
+
