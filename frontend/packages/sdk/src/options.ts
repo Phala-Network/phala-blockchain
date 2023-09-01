4ee4d0276017a0faef512051ffb1fd6382e5e9de
Leechael Yim <yanleech@gmail.com>
2023-09-02 07:27:57 +0800

    feat(jssdk): new pinkQuery implementation with ApiPromise

--- frontend/packages/sdk/src/options.ts
+++ frontend/packages/sdk/src/options.ts
@@ -3,9 +3,10 @@ import type { RegistryTypes } from "@polkadot/types/types";
 import { TypeRegistry } from "@polkadot/types";
 
 import { typeDefinitions } from '@polkadot/types';
+import SubstrateLookupTypes from '@polkadot/types-augment/lookup/substrate';
 import { types } from './lib/types';
 
-export const phalaRegistryTypes = { ...types, ...typeDefinitions } as unknown as RegistryTypes
+export const phalaRegistryTypes = { ...types, ...typeDefinitions, ...SubstrateLookupTypes } as unknown as RegistryTypes
 
 export const phalaTypes = new TypeRegistry()
 
