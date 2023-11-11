fe868dfe992402b23a9f93966fa6698438b5b0cc
Leechael Yim <yanleech@gmail.com>
2023-11-11 10:50:52 +0800

    refactor(jssdk): rename to provider avoid unecccessary cognition conflict

--- frontend/packages/sdk/src/providers/types.ts
+++ frontend/packages/sdk/src/providers/types.ts
@@ -3,12 +3,21 @@ import type { SubmittableExtrinsic } from '@polkadot/api/types'
 import type { Address } from 'viem'
 import type { CertificateData } from '../pruntime/certificate'
 
-export interface Signer {
+export interface Provider {
+  /**
+   * The SS58 format address to use for this provider.
+   */
   address: Readonly<Address>
 
+  /**
+   * Send an extrinsic to the network.
+   */
   send<TSubmittableResult extends SubmittableResult = SubmittableResult>(
     extrinsic: SubmittableExtrinsic<'promise'>
   ): Promise<TSubmittableResult>
 
+  /**
+   * Get a signed certificate from the account bind in the provider.
+   */
   signCertificate(ttl?: number): Promise<CertificateData>
 }
