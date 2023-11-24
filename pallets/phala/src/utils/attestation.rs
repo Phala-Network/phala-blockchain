a46c1b8ab480a3856e0c5c2fba504677e5619610
jasl <jasl9187@hotmail.com>
2023-11-24 08:29:32 +0800

    Revert "apply review suggest"
    This reverts commit b5c2329f26e8f713f3b2b7dfaaa39819728d07ad.

--- pallets/phala/src/utils/attestation.rs
+++ pallets/phala/src/utils/attestation.rs
@@ -110,31 +110,19 @@ impl IasFields {
 }
 
 pub fn validate(
-	attestation: Option<Box<AttestationReport>>,
+	attestation: Option<AttestationReport>,
 	user_data_hash: &[u8; 32],
 	now: u64,
 	verify_pruntime_hash: bool,
 	pruntime_allowlist: Vec<Vec<u8>>,
 	opt_out_enabled: bool,
 ) -> Result<ConfidentialReport, Error> {
-	let Some(boxed_attestation) = attestation else {
-		return if opt_out_enabled {
-			Ok(ConfidentialReport {
-				provider: None,
-				runtime_hash: Vec::new(),
-				confidence_level: 128u8,
-			})
-		} else {
-			Err(Error::NoneAttestationDisabled)
-		}
-	};
-
-	match *boxed_attestation {
-		AttestationReport::SgxIas {
+	match attestation {
+		Some(AttestationReport::SgxIas {
 			ra_report,
 			signature,
 			raw_signing_cert,
-		} => validate_ias_report(
+		}) => validate_ias_report(
 			user_data_hash,
 			ra_report.as_slice(),
 			signature.as_slice(),
@@ -143,7 +131,7 @@ pub fn validate(
 			verify_pruntime_hash,
 			pruntime_allowlist,
 		),
-		AttestationReport::SgxDcap { quote, collateral } => {
+		Some(AttestationReport::SgxDcap { quote, collateral }) => {
 			let Some(Collateral::SgxV30(collateral)) = collateral else {
 				return Err(Error::UnsupportedAttestationType);
 			};
@@ -155,6 +143,17 @@ pub fn validate(
 				verify_pruntime_hash,
 				pruntime_allowlist,
 			)
+		}
+		None => {
+			if opt_out_enabled {
+				Ok(ConfidentialReport {
+					provider: None,
+					runtime_hash: Vec::new(),
+					confidence_level: 128u8,
+				})
+			} else {
+				Err(Error::NoneAttestationDisabled)
+			}
 		}
 	}
 }
