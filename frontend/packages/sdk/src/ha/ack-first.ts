3aff34f88bb4d9252a77a7182bc261ad0d1b3caf
Leechael Yim <yanleech@gmail.com>
2023-11-25 02:20:39 +0800

    feat(jssdk): added three ha strategy

---
+++ frontend/packages/sdk/src/ha/ack-first.ts
@@ -0,0 +1,47 @@
+import { type ApiPromise } from '@polkadot/api'
+import type { Enum, Option, Text, U8aFixed, Vec } from '@polkadot/types'
+import createPruntimeClient from '../pruntime/createPruntimeClient'
+
+interface PhalaTypesVersionedWorkerEndpoints extends Enum {
+  readonly isV1: boolean
+  readonly asV1: Vec<Text>
+  readonly type: 'V1'
+}
+
+async function ack(
+  workerId: string,
+  endpoint: string
+): Promise<Readonly<[string, ReturnType<typeof createPruntimeClient>]>> {
+  const client = createPruntimeClient(endpoint)
+  const info = await client.getInfo({})
+  const actually = `0x${info.ecdhPublicKey || ''}`
+  if (actually === workerId) {
+    return [workerId, client] as const
+  }
+  throw new Error('On-chain worker ID not match to the worker ECDH PublicKey.')
+}
+
+/**
+ * This is the most simple strategy which pulling list of workers and check their available
+ * or not. Return first one that ack succeed.
+ */
+export function ackFirst() {
+  return async function ackFirst(
+    apiPromise: ApiPromise,
+    clusterId: string
+  ): Promise<Readonly<[string, ReturnType<typeof createPruntimeClient>]>> {
+    const workersQuery = await apiPromise.query.phalaPhatContracts.clusterWorkers<Vec<U8aFixed>>(clusterId)
+    const workerIds = workersQuery.map((i) => i.toHex())
+    const endpointsQuery =
+      await apiPromise.query.phalaRegistry.endpoints.multi<Option<PhalaTypesVersionedWorkerEndpoints>>(workerIds)
+    const pairs = endpointsQuery
+      .map((i, idx) => [workerIds[idx], i] as const)
+      .filter(([_, maybeEndpoint]) => maybeEndpoint.isSome)
+      .map(([workerId, maybeEndpoint]) => [workerId, maybeEndpoint.unwrap().asV1[0].toString()])
+    try {
+      return await Promise.any(pairs.map(([workerId, endpoint]) => ack(workerId, endpoint)))
+    } catch (_err) {
+      throw new Error('No worker available.')
+    }
+  }
+}
---
+++ frontend/packages/sdk/src/ha/periodicity-checker.ts
@@ -0,0 +1,30 @@
+import { type ApiPromise } from '@polkadot/api'
+import fetch from 'cross-fetch'
+import createPruntimeClient from '../pruntime/createPruntimeClient'
+
+/**
+ * This strategy is not rely on on-chain worker list avoiding extract RPC community. Instead of
+ * this, it fetch worker list from remote JSON file.
+ *
+ * It useful when you work with off-chain queries only and elimate the on-chain RPC dependency.
+ */
+
+const PRUNTIME_NODE_LIST = 'https://phala-network.github.io/pruntime-node-list/nodes.json'
+
+export function periodicityChecker(url?: string) {
+  return async function (
+    _apiPromise: ApiPromise,
+    clusterId: string
+  ): Promise<Readonly<[string, ReturnType<typeof createPruntimeClient>]>> {
+    const resp = await fetch(url || PRUNTIME_NODE_LIST)
+    const data = await resp.json()
+    const endpoints = data?.[clusterId] || []
+    if (endpoints.length === 0) {
+      throw new Error('No worker available.')
+    }
+    const picked = endpoints[Math.floor(Math.random() * endpoints.length)]
+    const client = createPruntimeClient(picked)
+    const info = await client.getInfo({})
+    return [`0x${info.ecdhPublicKey || ''}`, client] as const
+  }
+}
