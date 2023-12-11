a1d1660928c9d045e284f372d7f36c5d9abe966d
Kevin Wang <wy721@qq.com>
2023-12-11 06:57:57 +0000

    sidevm-sdk: Don't export sidevm_poll when without sidevm::main

--- crates/sidevm/macro/src/lib.rs
+++ crates/sidevm/macro/src/lib.rs
@@ -1,4 +1,5 @@
 use proc_macro::TokenStream;
+use syn::Result;
 
 mod macro_main;
 mod macro_ocall;
@@ -14,4 +15,25 @@ pub fn ocall(_: TokenStream, input: TokenStream) -> TokenStream {
 #[proc_macro_attribute]
 pub fn main(_: TokenStream, input: TokenStream) -> TokenStream {
     macro_main::patch(syn::parse_macro_input!(input)).into()
+}
+
+#[cfg(not(test))]
+fn find_crate_name(origin: &str) -> Result<syn::Ident> {
+    use proc_macro2::Span;
+    use proc_macro_crate::{crate_name, FoundCrate};
+    let name = match crate_name(origin) {
+        Ok(FoundCrate::Itself) => syn::Ident::new("crate", Span::call_site()),
+        Ok(FoundCrate::Name(alias)) => syn::Ident::new(&alias, Span::call_site()),
+        Err(e) => {
+            return Err(syn::Error::new(Span::call_site(), &e));
+        }
+    };
+    Ok(name)
+}
+
+#[cfg(test)]
+fn find_crate_name(origin: &str) -> Result<syn::Ident> {
+    use heck::ToSnakeCase;
+    use proc_macro2::Span;
+    Ok(syn::Ident::new(&origin.to_snake_case(), Span::call_site()))
 }
--- crates/sidevm/macro/src/macro_main.rs
+++ crates/sidevm/macro/src/macro_main.rs
@@ -10,7 +10,12 @@ pub(crate) fn patch(input: TokenStream) -> TokenStream {
 fn patch_or_err(input: TokenStream) -> syn::Result<TokenStream> {
     let main_fn: syn::ItemFn = syn::parse2(input.clone())?;
     let main_ident = &main_fn.sig.ident;
+    let crate_sidevm = crate::find_crate_name("sidevm")?;
     Ok(syn::parse_quote! {
+        #[no_mangle]
+        extern "C" fn sidevm_poll() -> i32 {
+            #crate_sidevm::env::tasks::sidevm_poll()
+        }
         #[no_mangle]
         fn sidevm_main_future() -> std::pin::Pin<std::boxed::Box<dyn std::future::Future<Output = ()>>> {
 
