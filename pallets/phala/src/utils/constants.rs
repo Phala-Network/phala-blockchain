f29bb8184339416085a3adffff4fec1879c8eff2
Jun Jiang <jasl9187@hotmail.com>
2023-12-23 17:19:53 +0800

    Correct DCAP constants

--- pallets/phala/src/utils/constants.rs
+++ pallets/phala/src/utils/constants.rs
@@ -1,21 +1,32 @@
 pub const RANDOMNESS_SUBJECT: &[u8] = b"PhalaPoW";
 
-pub const SGX_QUOTE_STATUS_LEVEL_1: &[&str] = &["OK"];
-pub const SGX_QUOTE_STATUS_LEVEL_2: &[&str] = &["SW_HARDENING_NEEDED"];
+pub const SGX_QUOTE_STATUS_LEVEL_1: &[&str] = &[
+	// IAS
+	"OK",
+	// DCAP
+	"UpToDate",
+];
+pub const SGX_QUOTE_STATUS_LEVEL_2: &[&str] = &[
+	// IAS
+	"SW_HARDENING_NEEDED",
+	// DCAP
+	"SWHardeningNeeded",
+];
 pub const SGX_QUOTE_STATUS_LEVEL_3: &[&str] = &[
 	// IAS
 	"CONFIGURATION_NEEDED",
 	"CONFIGURATION_AND_SW_HARDENING_NEEDED",
 	// DCAP
-	"CONFIG_NEEDED",
-	"CONFIG_AND_SW_HARDENING_NEEDED",
+	"ConfigurationNeeded",
+	"ConfigurationAndSWHardeningNeeded",
 ];
 // LEVEL 4 is LEVEL 3 with advisors which not included in whitelist
 pub const SGX_QUOTE_STATUS_LEVEL_5: &[&str] = &[
 	// IAS
 	"GROUP_OUT_OF_DATE",
 	// DCAP
-	"OUT_OF_DATE"
+	"OutOfDate",
+	"OutOfDateConfigurationNeeded",
 ];
 pub const SGX_QUOTE_ADVISORY_ID_WHITELIST: &[&str] = &[
 	"INTEL-SA-00334",
