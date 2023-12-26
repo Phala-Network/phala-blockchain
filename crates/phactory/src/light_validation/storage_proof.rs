3ced920d96997dd0b277b9b21e6342b8dafecd9a
Kevin Wang <wy721@qq.com>
2023-12-26 03:48:11 +0000

    pruntime: Read the new authorities first

--- crates/phactory/src/light_validation/storage_proof.rs
+++ crates/phactory/src/light_validation/storage_proof.rs
@@ -20,8 +20,6 @@ use anyhow::Result;
 use hash_db::{HashDB, Hasher, EMPTY_PREFIX};
 use trie::{trie_types::TrieDB, MemoryDB, Trie};
 
-use super::Error;
-
 pub(crate) type StorageProof = Vec<Vec<u8>>;
 
 /// This struct is used to read storage values from a subset of a Merklized database. The "proof"
@@ -59,7 +57,7 @@ where
         self.trie()?
             .get(key)
             .map(|value| value.map(|value| value.to_vec()))
-            .map_err(|_| anyhow::Error::msg(Error::StorageValueUnavailable))
+            .map_err(|err| anyhow::anyhow!("Failed to read value from trie: {err:?}"))
     }
 
     fn trie(&self) -> Result<TrieDB<H>> {
