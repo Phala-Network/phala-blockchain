4e1f08d29e60b0cadbc20cd9debd5f46bb7f8fe8
Kevin Wang <wy721@qq.com>
2024-01-05 01:33:21 +0000

    Fix tests

--- standalone/runtime/src/lib.rs
+++ standalone/runtime/src/lib.rs
@@ -138,7 +138,7 @@ include!(concat!(env!("OUT_DIR"), "/wasm_binary.rs"));
 /// Max size for serialized extrinsic params for this testing runtime.
 /// This is a quite arbitrary but empirically battle tested value.
 #[cfg(test)]
-pub const CALL_PARAMS_MAX_SIZE: usize = 300;
+pub const CALL_PARAMS_MAX_SIZE: usize = 512;
 
 /// Wasm binary unwrapped. If built with `SKIP_WASM_BUILD`, the function panics.
 #[cfg(all(feature = "std", feature = "include-wasm"))]
