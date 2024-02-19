7207132c9415aeeddc4b9268505ce89ea973ff85
Kevin Wang <wy721@qq.com>
2024-02-19 02:05:17 +0000

    Handle http error in getCollateral.js

--- e2e/contracts/check_system/getCollateral.js
+++ e2e/contracts/check_system/getCollateral.js
@@ -1,55 +1,52 @@
-async function getCollateral(baseUrl, fmspc) {
-    const headers = { 'Accept': 'application/json' };
-    const fetchOptions = {
-        method: 'GET',
-        headers: headers,
-    };
-
-    try {
-        // PCK CRL
-        let pckCrlResponse = await fetch(`${baseUrl}/pckcrl?ca=processor`, fetchOptions);
-        const pckCrlIssuerChain = pckCrlResponse.headers.get("SGX-PCK-CRL-Issuer-Chain");
-        const pckCrl = await pckCrlResponse.text();
-
-        // Root CA CRL
-        let rootCaCrlResponse = await fetch(`${baseUrl}/rootcacrl`, fetchOptions);
-        const rootCaCrl = await rootCaCrlResponse.text();
-
-        // TCB Info
-        let tcbInfoResponse = await fetch(`${baseUrl}/tcb?fmspc=${fmspc}`, fetchOptions);
-        const tcbInfoIssuerChain = tcbInfoResponse.headers.get("SGX-TCB-Info-Issuer-Chain") || tcbInfoResponse.headers.get("TCB-Info-Issuer-Chain");
-        const rawTcbInfo = await tcbInfoResponse.text();
-
-        // QE Identity
-        let qeIdentityResponse = await fetch(`${baseUrl}/qe/identity`, fetchOptions);
-        const qeIdentityIssuerChain = qeIdentityResponse.headers.get("SGX-Enclave-Identity-Issuer-Chain");
-        const rawQeIdentity = await qeIdentityResponse.text();
-
-        // Parse JSON Data
-        const tcbInfoJson = JSON.parse(rawTcbInfo);
-        const tcbInfo = JSON.stringify(tcbInfoJson["tcbInfo"]);
-        const tcbInfoSignature = Sidevm.hexDecode(tcbInfoJson["signature"]);
-
-        const qeIdentityJson = JSON.parse(rawQeIdentity);
-        const qeIdentity = JSON.stringify(qeIdentityJson["enclaveIdentity"]);
-        const qeIdentitySignature = Sidevm.hexDecode(qeIdentityJson["signature"]);
-
-        // Construct result object
-        return {
-            pckCrlIssuerChain,
-            rootCaCrl,
-            pckCrl,
-            tcbInfoIssuerChain,
-            tcbInfo,
-            tcbInfoSignature,
-            qeIdentityIssuerChain,
-            qeIdentity,
-            qeIdentitySignature
-        };
-    } catch (error) {
-        console.error("Error during fetch:", error);
-        throw error;
+async function httpGet(url) {
+    const response = await fetch(url);
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
     }
+    return response;
+}
+
+async function getCollateral(baseUrl, fmspc) {
+    // PCK CRL
+    let pckCrlResponse = await httpGet(`${baseUrl}/pckcrl?ca=processor`);
+    const pckCrlIssuerChain = pckCrlResponse.headers.get("SGX-PCK-CRL-Issuer-Chain");
+    const pckCrl = await pckCrlResponse.text();
+
+    // Root CA CRL
+    let rootCaCrlResponse = await httpGet(`${baseUrl}/rootcacrl`);
+    const rootCaCrl = await rootCaCrlResponse.text();
+
+    // TCB Info
+    let tcbInfoResponse = await httpGet(`${baseUrl}/tcb?fmspc=${fmspc}`);
+    const tcbInfoIssuerChain = tcbInfoResponse.headers.get("SGX-TCB-Info-Issuer-Chain") || tcbInfoResponse.headers.get("TCB-Info-Issuer-Chain");
+    const rawTcbInfo = await tcbInfoResponse.text();
+
+    // QE Identity
+    let qeIdentityResponse = await httpGet(`${baseUrl}/qe/identity`);
+    const qeIdentityIssuerChain = qeIdentityResponse.headers.get("SGX-Enclave-Identity-Issuer-Chain");
+    const rawQeIdentity = await qeIdentityResponse.text();
+
+    // Parse JSON Data
+    const tcbInfoJson = JSON.parse(rawTcbInfo);
+    const tcbInfo = JSON.stringify(tcbInfoJson["tcbInfo"]);
+    const tcbInfoSignature = Sidevm.hexDecode(tcbInfoJson["signature"]);
+
+    const qeIdentityJson = JSON.parse(rawQeIdentity);
+    const qeIdentity = JSON.stringify(qeIdentityJson["enclaveIdentity"]);
+    const qeIdentitySignature = Sidevm.hexDecode(qeIdentityJson["signature"]);
+
+    // Construct result object
+    return {
+        pckCrlIssuerChain,
+        rootCaCrl,
+        pckCrl,
+        tcbInfoIssuerChain,
+        tcbInfo,
+        tcbInfoSignature,
+        qeIdentityIssuerChain,
+        qeIdentity,
+        qeIdentitySignature
+    };
 }
 
 async function main() {
@@ -75,5 +72,8 @@ async function main() {
 
 main()
     .then(result => scriptOutput = result)
-    .catch(error => scriptOutput = error)
+    .catch(error => {
+        console.error(`Error: ${error}`);
+        scriptOutput = error
+    })
     .finally(() => process.exit());
